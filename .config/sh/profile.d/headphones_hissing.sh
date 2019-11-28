e650dacba6d739d4c3417de9a62f073929a34579
Benedikt Rips <benedikt.rips@gmail.com>
2019-11-28 12:14:52 +0100

    Remove headphones_hissing.sh
    The problem of hissing headphones is already handled by pacman hooks.

--- .config/sh/profile.d/headphones_hissing.sh
+++
@@ -1,6 +0,0 @@
-# headphones_hissing.sh
-
-# Sets the headphone microphone boost volume to 10dB to disable the hissing
-# sound when using headphones.
-
-#amixer -c PCH cset 'name=Headphone Mic Boost Volume' 1 >/dev/null

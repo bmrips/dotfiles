dc191f600798bcb5b9b90bd360c09b7cf9ea97bb
Benedikt Rips <benedikt.rips@gmail.com>
2020-02-29 15:43:05 +0100

    Add configuration for ls_colors

---
+++ .config/sh/rc.d/ls_colors.sh
@@ -0,0 +1,12 @@
+# Find configuration file
+for colors in {$XDG_CONFIG_HOME,/etc}/ls_colors/ls_colors.conf; do
+	if [[ -r $colors ]]; then
+		COLORS="$colors"
+		break
+	fi
+done
+
+# Exexcute dircolors if $COLORS file exists
+if [[ -n $COLORS ]]; then
+    eval "`dircolors --bourne-shell "$COLORS" 2>/dev/null`"
+fi

1411bf1e53a607aaf1b9a6eeadccd1771b5b4417
Benedikt Rips <benedikt.rips@gmail.com>
2022-04-29 21:03:07 +0200

    Disable treesitter syntax highlighting in insert mode
    In large Haskell files, treesitter syntax highlighting causes lags.
    Hence it is turned off in insert mode.

--- .config/nvim/after/ftplugin/haskell.lua
+++ .config/nvim/after/ftplugin/haskell.lua
@@ -9,3 +9,12 @@ opt.suffixesadd = { ".hs", ".lhs" }
 
 vim.b.undo_ftplugin = vim.b.undo_ftplugin ..
   "| set iskeyword< shiftwidth< softtabstop< suffixesadd<"
+
+-- Disable treesitter syntax highlighting in insert mode
+vim.api.nvim_create_augroup("haskell", {})
+vim.api.nvim_create_autocmd("InsertEnter", {
+  group = "haskell",
+  buffer = 0,
+  desc = "Disable treesitter syntax highlighting in insert mode",
+  callback = function () require("nvim-treesitter.highlight").stop("%") end,
+})

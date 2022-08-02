7da306dcd35f3c94a55cceadc4345bbe93423864
Benedikt Rips <benedikt.rips@gmail.com>
2022-08-02 12:06:52 +0200

    Add the Lua language server

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -203,6 +203,28 @@ local plugins = {
         }
       }
 
+      -- Lua
+      require'lspconfig'.sumneko_lua.setup {
+        settings = {
+          Lua = {
+            runtime = {
+              version = "LuaJIT",
+            },
+            diagnostics = {
+              -- Get the language server to recognize the `vim` global
+              globals = { "vim" },
+            },
+            workspace = {
+              -- Make the server aware of Neovim runtime files
+              library = vim.api.nvim_get_runtime_file("", true),
+            },
+            telemetry = {
+              enable = false,
+            },
+          },
+        },
+      }
+
       -- TeX
       lspconfig.texlab.setup {
         settings = {

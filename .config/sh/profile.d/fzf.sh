3603bc1b95aba2b6cddc6160895e31a33e2f387e
Benedikt Rips <benedikt.rips@gmail.com>
2022-09-01 00:13:17 +0200

    Toggle the listing of hidden and ignored files in Fzf

--- .config/sh/profile.d/fzf.sh
+++ .config/sh/profile.d/fzf.sh
@@ -1,6 +1,6 @@
 # shellcheck shell=bash
 
-export FZF_DEFAULT_COMMAND='fd --type f --hidden'
+export FZF_DEFAULT_COMMAND="fd --type file --hidden"
 export FZF_DEFAULT_OPTS="--layout=reverse\
  --border=horizontal\
  --marker=❯\
@@ -10,12 +10,12 @@ export FZF_DEFAULT_OPTS="--layout=reverse\
  --preview-window=right,border,hidden\
  --bind='alt-a:toggle-all,f3:toggle-preview-wrap,f4:toggle-preview,f5:change-preview-window(nohidden,down|nohidden,left|nohidden,up|nohidden,right)'"
 
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_CTRL_T_OPTS="--preview='bat --plain --color=always --paging=never {}'"
+export FZF_CTRL_T_COMMAND="fzf-state get-source files"
+export FZF_CTRL_T_OPTS="--preview='bat --plain --color=always --paging=never {}' --bind='$(fzf-state binds "$FZF_CTRL_T_COMMAND")'"
 
-export FZF_ALT_C_COMMAND='fd --type d --hidden'
+export FZF_ALT_C_COMMAND="fzf-state get-source directories"
 export DIR_PREVIEW="ls -l --human-readable --color=always --group-directories-first --time-style=+t --literal {} | cut --delimiter=\" \" --fields=1,5- | sed \"s/ t / /\" | tail -n+2"
-export FZF_ALT_C_OPTS="--preview='$DIR_PREVIEW'"
+export FZF_ALT_C_OPTS="--preview='$DIR_PREVIEW' --bind='$(fzf-state binds "$FZF_ALT_C_COMMAND")'"
 
 # Path and directory completion, e.g. for `cd .config/**`
 _fzf_compgen_path() {

578d9e6dcbb6f4ac31bc30a169b837f9c8c3a26f
Benedikt Rips <benedikt.rips@gmail.com>
2022-09-02 15:19:10 +0200

    In Zsh, interactively go to a directory in the cd history

--- .config/zsh/rc.d/fzf.zsh
+++ .config/zsh/rc.d/fzf.zsh
@@ -12,6 +12,27 @@ zstyle ':completion:*' fzf-search-display true # Search completion descriptions
 # eval twice, first to unescape the string, second to expand the $variable
 zstyle ':completion::*:(-command-|-parameter-|-brace-parameter-|export|unset|expand):*' fzf-completion-opts --preview='eval eval echo {1}' --preview-window=wrap
 
+#[ Go to directory in cd history ]#
+FZF_CDHIST_OPTS="--preview='$DIR_PREVIEW'"
+
+function fzf-cdhist-widget() {
+    dir="$(FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS $FZF_CDHIST_OPTS" fzf <$HOME/.cache/cd_history)"
+  if [[ -z "$dir" ]]; then
+    zle redisplay
+    return 0
+  fi
+  zle push-line # Clear buffer. Auto-restored on next prompt.
+  BUFFER="cd -- ${(q)dir}"
+  zle accept-line
+  local ret=$?
+  unset dir # ensure this doesn't end up appearing in prompt expansion
+  zle reset-prompt
+  return $ret
+}
+
+zle -N fzf-cdhist-widget
+bindkey "^Y" fzf-cdhist-widget
+
 #[ Interactive grep ]#
 FZF_GREP_COMMAND="fzf-state get-source grep"
 FZF_GREP_PREVIEW="bat --plain --color=always --paging=never --line-range=\$(fzf-state context {2}): --highlight-line={2} {1}"

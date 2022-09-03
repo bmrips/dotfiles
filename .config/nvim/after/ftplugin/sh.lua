721200f7b22eb0784fc5c674e1448a8d65c031da
Benedikt Rips <benedikt.rips@gmail.com>
2022-09-03 09:55:37 +0200

    Enable soft wrap in shell scripts

--- .config/nvim/after/ftplugin/sh.lua
+++ .config/nvim/after/ftplugin/sh.lua
@@ -4,6 +4,7 @@ opt.comments = ":#"
 opt.commentstring = "# %s"
 opt.formatoptions:remove("t")
 opt.formatoptions:append("l")
+opt.wrap = true
 
 vim.b.undo_ftplugin = vim.b.undo_ftplugin ..
   "| set comments< commentstring< formatoptions<"

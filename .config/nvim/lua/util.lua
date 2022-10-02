edef669bc144d0522b590e7aba96339b8c962d33
Benedikt Rips <benedikt.rips@gmail.com>
2022-10-02 13:53:45 +0200

    Remove deprecated utility functions

--- .config/nvim/lua/util.lua
+++ .config/nvim/lua/util.lua
@@ -36,21 +36,6 @@ M.reindent = function(info)
   vim.fn.winrestview(view)
 end
 
--- An alias for vim.api.nvim_set_keymap, not remapping by default.
-M.map = function(mode, lhs, rhs, opts)
-  local options = { noremap = true }
-  if opts then options = vim.tbl_extend('force', options, opts) end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
-
--- An alias for vim.api.nvim_buf_set_keymap for the current buffer, not
--- remapping by default.
-M.buf_map = function(mode, lhs, rhs, opts)
-  local options = { noremap = true }
-  if opts then options = vim.tbl_extend('force', options, opts) end
-  vim.api.nvim_buf_set_keymap(0, mode, lhs, rhs, options)
-end
-
 -- An alias for vim.api.nvim_replace_termcodes.
 M.termcode = function(str)
   return vim.api.nvim_replace_termcodes(str, true, true, true)

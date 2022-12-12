c650664fa41199c55cafc331249b8b3830a9e601
Benedikt Rips <benedikt.rips@gmail.com>
2022-12-12 07:48:36 +0100

    fix(nvim): consistent whitespace settings
    Always expand tabs and prefer to adjust `'tabstop'` over `'shiftwidth'`
    because the latter defaults to the former.

--- .config/nvim/after/ftplugin/haskell.lua
+++ .config/nvim/after/ftplugin/haskell.lua
@@ -2,8 +2,7 @@ local opt = vim.opt_local
 
 opt.includeexpr = "substitute(v:fname, '\\.', '/', 'g')"
 opt.iskeyword:append "'"
-opt.shiftwidth = 2
+opt.tabstop = 2
 opt.suffixesadd = { '.hs', '.lhs' }
 
-vim.b.undo_ftplugin = vim.b.undo_ftplugin
-  .. '| set includeexpr< iskeyword< shiftwidth< suffixesadd<'
+vim.b.undo_ftplugin = vim.b.undo_ftplugin .. '| set includeexpr< iskeyword< tabstop< suffixesadd<'
--- .config/nvim/after/ftplugin/vim.lua
+++ .config/nvim/after/ftplugin/vim.lua
@@ -5,7 +5,7 @@ opt.formatoptions:remove 'r'
 opt.formatoptions:remove 'o'
 opt.include = '^\\s*\\%(source\\|runtime\\%)'
 opt.path = opt.runtimepath:get()
-opt.shiftwidth = 2
+opt.tabstop = 2
 
 vim.b.undo_ftplugin = vim.b.undo_ftplugin
   .. '| set comments< formatoptions< include< path< tabstop<'

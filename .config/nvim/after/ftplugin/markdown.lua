5376ee8aef5d274ceaea3eaac5573cb3bf6799f5
Benedikt Rips <benedikt.rips@gmail.com>
2022-12-12 07:43:19 +0100

    feat(nvim): in markdown, indent with two spaces

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -1,3 +1,9 @@
+local opt = vim.opt_local
+
+opt.tabstop = 2
+
+vim.b.undo_ftplugin = vim.b.undo_ftplugin .. '| set tabstop<'
+
 if not vim.g.no_plugin_maps then
   --stylua: ignore
   require('nest').applyKeymaps {

8e4bac42abc343edab400bc9c5dd62ffefacf289
Benedikt Rips <benedikt.rips@gmail.com>
2023-01-08 11:36:29 +0100

    feat(sh): set `XDG_STATE_HOME`

--- .config/sh/rc.sh
+++ .config/sh/rc.sh
@@ -7,6 +7,7 @@ export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_DATA_DIRS="/usr/local/share:/usr/share"
+export XDG_STATE_HOME="$HOME/.local/state"
 
 #[ Aliases ]# {{{1
 

4df5361163126d572dc17ef3cdcdab4421ee405f
Benedikt Rips <benedikt.rips@gmail.com>
2023-01-17 22:26:09 +0100

    feat(nvim): insert `$` in pairs in markdown files

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -4,6 +4,10 @@ opt.tabstop = 2
 
 vim.b.undo_ftplugin = vim.b.undo_ftplugin .. '| set tabstop<'
 
+-- Automatically insert $ in a pair
+vim.b.AutoPairs = vim.tbl_extend('force', vim.g.AutoPairs, { ['$'] = '$' })
+vim.b.undo_ftplugin = vim.b.undo_ftplugin .. '| unlet b:AutoPairs'
+
 if not vim.g.no_plugin_maps then
   --stylua: ignore
   require('nest').applyKeymaps {

f174e36862b8f0427beab49dcbc13c5936b04a1a
Benedikt Rips <benedikt.rips@gmail.com>
2023-02-03 13:17:46 +0100

    fix(nvim): prefer a vertical preview split for Fzf

--- .config/nvim/lua/config/plugins/fzf-lua.lua
+++ .config/nvim/lua/config/plugins/fzf-lua.lua
@@ -5,6 +5,11 @@ return {
     'vijaymarupudi/nvim-fzf',
   },
   opts = {
+    winopts = {
+      preview = {
+        flip_columns = 150,
+      },
+    },
     lsp = {
       code_actions = {
         winopts = { relative = 'cursor' },

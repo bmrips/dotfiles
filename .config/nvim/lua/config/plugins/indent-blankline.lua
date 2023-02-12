d3a2ce16f3fb79149a4104a05e504c0b656ad90e
Benedikt Rips <benedikt.rips@gmail.com>
2023-02-12 11:18:23 +0100

    feat(nvim): add the indent-blankline plugin

---
+++ .config/nvim/lua/config/plugins/indent-blankline.lua
@@ -0,0 +1,12 @@
+return {
+  'lukas-reineke/indent-blankline.nvim',
+  opts = {
+    char = '⏐',
+    filetype_exclude = {
+      'help',
+      'markdown',
+    },
+    show_current_context = true,
+    use_treesitter = true,
+  },
+}

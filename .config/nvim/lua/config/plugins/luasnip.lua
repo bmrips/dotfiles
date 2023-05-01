9ad5385cd7aec6a12c345a198b00267ee1203a06
Benedikt Rips <benedikt.rips@gmail.com>
2023-05-01 08:57:44 +0200

    fix(nvim): load friendly-snippets

--- .config/nvim/lua/config/plugins/luasnip.lua
+++ .config/nvim/lua/config/plugins/luasnip.lua
@@ -2,4 +2,8 @@ return {
   'L3MON4D3/LuaSnip',
   build = 'make install_jsregexp', -- for LSP snippet transformations
   dependencies = 'rafamadriz/friendly-snippets',
+  config = function(_, opts)
+    require('luasnip').setup(opts)
+    require('luasnip.loaders.from_vscode').lazy_load()
+  end,
 }

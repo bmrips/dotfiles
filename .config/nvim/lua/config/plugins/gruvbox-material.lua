f40c55f7b37cc383cbf034d9b623709dea0d1966
Benedikt Rips <benedikt.rips@gmail.com>
2023-07-28 22:33:50 +0200

    feat(nvim): override gruvbox-material search color

--- .config/nvim/lua/config/plugins/gruvbox-material.lua
+++ .config/nvim/lua/config/plugins/gruvbox-material.lua
@@ -9,5 +9,14 @@ return {
     foreground = 'mix',
     sign_column_background = 'grey',
   },
-  config = require('compat.vimscript.plugin').setup 'gruvbox_material_',
+  config = function(_, opts)
+    require('compat.vimscript.plugin').setup 'gruvbox_material_'(nil, opts)
+    vim.api.nvim_create_autocmd('Colorscheme', {
+      pattern = 'gruvbox-material',
+      desc = 'Colorscheme overrides',
+      callback = function()
+        vim.api.nvim_set_hl(0, 'Search', { link = 'Substitute' })
+      end,
+    })
+  end,
 }

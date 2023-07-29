52a7186987539a5e54b2a9794394509c24885abe
Benedikt Rips <benedikt.rips@gmail.com>
2023-07-29 13:47:35 +0200

    fix(nvim): list treesitter dependency for endwise

--- .config/nvim/lua/config/plugins/nvim-treesitter-endwise.lua
+++ .config/nvim/lua/config/plugins/nvim-treesitter-endwise.lua
@@ -1,5 +1,6 @@
 return {
   'RRethy/nvim-treesitter-endwise',
+  dependencies = 'nvim-treesitter/nvim-treesitter',
   config = function()
     require('nvim-treesitter.configs').setup {
       endwise = {

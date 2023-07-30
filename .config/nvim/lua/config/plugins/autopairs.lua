492c18844268d29d8895d957c22e1c0731665900
Benedikt Rips <benedikt.rips@gmail.com>
2023-07-30 16:57:59 +0200

    feat(nvim): insert spaces in specified auto-pairs

--- .config/nvim/lua/config/plugins/autopairs.lua
+++ .config/nvim/lua/config/plugins/autopairs.lua
@@ -9,7 +9,9 @@ return {
     autopairs.setup(opts)
 
     local Rule = require 'nvim-autopairs.rule'
+    local cond = require 'nvim-autopairs.conds'
     local ts_cond = require 'nvim-autopairs.ts-conds'
+
     -- stylua: ignore
     autopairs.add_rules {
       Rule('$', '$', { 'markdown', 'tex' })
@@ -25,5 +27,41 @@ return {
           return info.char == ']' and followed_by_end_pair
         end),
     }
+
+    -- When typing a space, insert a pair of spaces in specified pairs
+    local pairs_obeying_space = {
+      ['$'] = '$',
+      ['('] = ')',
+      ['['] = ']',
+      ['\\['] = '\\]',
+      ['{'] = '}',
+    }
+
+    -- stylua: ignore
+    autopairs.add_rules {
+      Rule(' ', ' ')
+        :with_pair(function(info)
+          for begin_pair, end_pair in pairs(pairs_obeying_space) do
+            local left = info.line:sub(1, info.col-1)
+            local right = info.line:sub(info.col)
+            if vim.endswith(left, begin_pair) and vim.startswith(right, end_pair) then
+              return true
+            end
+          end
+          return false
+        end)
+    }
+
+    -- stylua: ignore
+    for begin_pair, end_pair in pairs(pairs_obeying_space) do
+      autopairs.add_rules {
+        Rule(begin_pair..' ', ' '..end_pair)
+          :with_pair(cond.none())
+          :with_move(function(info)
+            return vim.startswith(info.line:sub(info.col), info.rule.end_pair)
+          end)
+          :use_key(end_pair:sub(-1))
+      }
+    end
   end,
 }

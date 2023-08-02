9a3992a6984b5c9cc5c66576ba6b63a2c0bdded6
Benedikt Rips <benedikt.rips@gmail.com>
2023-08-02 05:55:37 +0200

    refactor(nvim): use the `string.rep` function

--- .config/nvim/lua/config/foldtext.lua
+++ .config/nvim/lua/config/foldtext.lua
@@ -1,8 +1,5 @@
 return function(linenr)
   local line = vim.api.nvim_buf_get_lines(0, linenr - 1, linenr, true)[1]
-  local tabInSpaces = ''
-  for _ = 1, vim.opt.tabstop:get() do
-    tabInSpaces = tabInSpaces .. ' '
-  end
+  local tabInSpaces = string.rep(' ', vim.opt.tabstop:get())
   return line:gsub('%s*$', ''):gsub('\t', tabInSpaces)
 end

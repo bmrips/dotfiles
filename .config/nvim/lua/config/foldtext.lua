65bf2ccbdda7ac1d30067ced1ad3a53d73b12223
Benedikt Rips <benedikt.rips@gmail.com>
2023-08-02 05:58:09 +0200

    feat(nvim): append an ellipsis to folded lines

--- .config/nvim/lua/config/foldtext.lua
+++ .config/nvim/lua/config/foldtext.lua
@@ -1,5 +1,5 @@
 return function(linenr)
   local line = vim.api.nvim_buf_get_lines(0, linenr - 1, linenr, true)[1]
   local tabInSpaces = string.rep(' ', vim.opt.tabstop:get())
-  return line:gsub('%s*$', ''):gsub('\t', tabInSpaces)
+  return line:gsub('%s*$', ' ⋯'):gsub('\t', tabInSpaces)
 end

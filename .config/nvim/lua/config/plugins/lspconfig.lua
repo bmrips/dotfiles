c5417e064cdb5c39479236d3783661fa4e7599c3
Benedikt Rips <benedikt.rips@gmail.com>
2023-08-08 22:51:57 +0200

    chore(nvim): update lspconfig

--- .config/nvim/lua/config/plugins/lspconfig.lua
+++ .config/nvim/lua/config/plugins/lspconfig.lua
@@ -36,13 +36,27 @@ local servers = {
     },
   },
   lua_ls = {
-    settings = {
-      Lua = {
-        diagnostics = {
-          globals = { 'vim' },
-        },
-      },
-    },
+    on_init = function(client)
+      local path = client.workspace_folders[1].name
+      if
+        not vim.loop.fs_stat(path .. '/.luarc.json')
+        and not vim.loop.fs_stat(path .. '/.luarc.jsonc')
+      then
+        client.config.settings = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          runtime = {
+            version = 'LuaJIT',
+          },
+          -- Make the server aware of Neovim runtime files
+          workspace = {
+            library = vim.api.nvim_get_runtime_file('', true),
+          },
+        })
+
+        client.notify('workspace/didChangeConfiguration', { settings = client.config.settings })
+      end
+      return true
+    end,
   },
   marksman = {},
   pkgbuild_language_server = {},

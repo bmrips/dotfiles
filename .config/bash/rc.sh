bd4a19facae7484e1ccd1cf0e419397d1372a41f
Benedikt Rips <benedikt.rips@gmail.com>
2023-09-25 22:29:51 +0200

    feat: use starship as prompt

--- .config/bash/rc.sh
+++ .config/bash/rc.sh
@@ -34,35 +34,4 @@ HISTFILE="$XDG_STATE_HOME/bash/history"
 HISTCONTROL=ignoreboth
 shopt -s histappend
 
-#[ Prompt ]#
-
-# Set red color and '#' for root
-if [[ $(whoami) == root ]]; then
-    suffix="#"
-    color="$RED"
-else
-    suffix="$"
-    color="$GREEN"
-fi
-
-# First prompt
-PS1='$(tput sc; rightprompt; tput rc)${color}\u${RESET}@${BLUE}\h${RESET}:${MAGENTA}\w${RESET} ${suffix} '
-
-rightprompt() {
-    printf "%*s" $COLUMNS "$(__git_ps1 '(%s)') "
-}
-
-# Git prompt options
-GIT_PS1_SHOWDIRTYSTATE="on"     # + for staged, * if unstaged.
-GIT_PS1_SHOWSTASHSTATE="on"     # $ if something is stashed.
-GIT_PS1_SHOWUNTRACKEDFILES="on" # % if there are untracked files.
-GIT_PS1_SHOWUPSTREAM="on"       # <,>,<> behind, ahead, or diverged from upstream.
-
-# Second prompt
-ps2() {
-    ps1="${USER}@${HOSTNAME}:${PWD/$HOME/}${suffix} "
-    printf "%${#ps1}s" "> "
-}
-PS2='$(ps2)'
-
 load_plugins "$HOME/.config/bash/rc.d"
--- .config/zsh/rc.d/powerlevel10k.zsh
+++
@@ -1,5 +0,0 @@
-try_source \
-    "$HOME/.nix-profile/share/zsh-powerlevel10k/powerlevel10k.zsh-theme" \
-    "/usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme"
-
-source "$XDG_CONFIG_HOME/powerlevel10k.zsh"

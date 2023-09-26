4a11af4cd3eb0bbcf1d56a4b835edde8219f162f
Benedikt Rips <benedikt.rips@gmail.com>
2023-09-26 09:36:32 +0200

    feat(zsh): substitute ~ for $HOME in fzf

--- .config/zsh/rc.d/fzf.zsh
+++ .config/zsh/rc.d/fzf.zsh
@@ -42,7 +42,7 @@ bindkey '^B' fzf-goto-widget
 FZF_CDHIST_OPTS="--preview='$DIR_PREVIEW'"
 
 function fzf-cdhist-widget() {
-    local dir="$(FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS $FZF_CDHIST_OPTS" fzf <$XDG_STATE_HOME/cd_history)"
+    local dir="$(sed "s#$HOME#~#" $XDG_STATE_HOME/cd_history | FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS $FZF_CDHIST_OPTS" fzf | sed "s#~#$HOME#")"
     if [[ -z "$dir" ]]; then
         zle redisplay
         return 0

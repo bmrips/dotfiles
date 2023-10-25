4eaccc054977db34c0131cb29dabd2e7618dc33b
Benedikt Rips <benedikt.rips@gmail.com>
2023-10-25 23:02:57 +0200

    fix(nvim): lift plugin version restriction

--- .config/nvim/lua/config/plugins/markdown-preview.lua
+++ .config/nvim/lua/config/plugins/markdown-preview.lua
@@ -1,7 +1,6 @@
 return {
   'iamcco/markdown-preview.nvim',
   build = 'env --chdir app npm install',
-  commit = '239ea074',
   ft = 'markdown',
   opts = {
     -- Do not close the current preview when changing the buffer.

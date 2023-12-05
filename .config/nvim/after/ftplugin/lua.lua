5830965002c486bf79d2c06356121a1e9e776890
Benedikt Rips <benedikt.rips@gmail.com>
2023-12-05 17:32:04 +0100

    feat(nvim): configure inclusion patterns

--- .config/nvim/after/ftplugin/lua.lua
+++ .config/nvim/after/ftplugin/lua.lua
@@ -1,6 +1,8 @@
 local opt = vim.opt_local
 
+opt.includeexpr = "substitute(v:fname, '\\.', '/', 'g')"
 opt.omnifunc = 'v:lua.vim.lua_omnifunc'
+opt.suffixesadd = { '.lua' }
 opt.tabstop = 2
 
-vim.b.undo_ftplugin = vim.b.undo_ftplugin .. '| set omnifunc< tabstop<'
+vim.b.undo_ftplugin = vim.b.undo_ftplugin .. '| set includeexpr< omnifunc< suffixesadd< tabstop<'
--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -3,9 +3,11 @@ local opt = vim.opt_local
 opt.joinspaces = false
 opt.shiftwidth = 0
 opt.softtabstop = 0
+opt.suffixesadd = { '.md', 'markdown' }
 opt.tabstop = 2
 
-vim.b.undo_ftplugin = vim.b.undo_ftplugin .. '| set joinspaces< shiftwidth< softtabstop< tabstop<'
+vim.b.undo_ftplugin = vim.b.undo_ftplugin
+  .. '| set joinspaces< shiftwidth< softtabstop< suffixesadd< tabstop<'
 
 if not vim.g.no_plugin_maps then
   --stylua: ignore

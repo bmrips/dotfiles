d7a6cce8a09700ef7609ee6906fe146d1c421c01
Benedikt Rips <benedikt.rips@gmail.com>
2023-12-15 21:47:59 +0100

    refactor(nvim): store LTeX data in subdirectory

--- .config/nvim/lua/config/plugins/lspconfig.lua
+++ .config/nvim/lua/config/plugins/lspconfig.lua
@@ -35,6 +35,7 @@ local servers = {
     on_attach = function()
       require('ltex_extra').setup {
         load_langs = { 'en-US', 'de-DE' },
+        path = '.ltex',
       }
     end,
     settings = {

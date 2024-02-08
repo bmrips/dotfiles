06b28baea2d3dbc9ca47040dd9063cda9fb3d4ef
Benedikt Rips <benedikt.rips@gmail.com>
2024-02-09 00:10:54 +0100

    fix(sh): initialise XDG_DATA_DIRS by system

--- .config/sh/profile.sh
+++ .config/sh/profile.sh
@@ -7,7 +7,6 @@ prepend_to_path PATH "$HOME/.local/bin"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
-export XDG_DATA_DIRS="/usr/local/share:/usr/share"
 export XDG_STATE_HOME="$HOME/.local/state"
 
 # Add Nix' share directory

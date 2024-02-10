6200383a704275f20f75705a1ff6a4114cb72f80
Benedikt Rips <benedikt.rips@gmail.com>
2024-02-10 12:53:29 +0100

    feat(nvim): in Lua, search in `lua/` imports

--- .config/nvim/after/ftplugin/lua.lua
+++ .config/nvim/after/ftplugin/lua.lua
@@ -2,7 +2,9 @@ local opt = vim.opt_local
 
 opt.includeexpr = "substitute(v:fname, '\\.', '/', 'g')"
 opt.omnifunc = 'v:lua.vim.lua_omnifunc'
+opt.path:append 'lua/'
 opt.suffixesadd = { '.lua' }
 opt.tabstop = 2
 
-vim.b.undo_ftplugin = vim.b.undo_ftplugin .. '| set includeexpr< omnifunc< suffixesadd< tabstop<'
+vim.b.undo_ftplugin = vim.b.undo_ftplugin
+  .. '| set includeexpr< omnifunc< path< suffixesadd< tabstop<'

cd220fc8802342c19e5feb35ff20f80857dd70e5
Benedikt Rips <benedikt.rips@gmail.com>
2024-02-21 23:34:53 +0100

    fix(nixpkgs): pre-commit with meta hooks bug
    See https://github.com/NixOS/nixpkgs/pull/251418.

---
+++ .config/nixpkgs/overlays/pre-commit_with_meta_hooks.nix
@@ -0,0 +1,13 @@
+final: prev:
+
+{
+  pre-commit = prev.pre-commit.overrideAttrs (prevAttrs:
+    with final.python3Packages;
+    let depsPath = makePythonPath prevAttrs.propagatedBuildInputs;
+    in {
+      makeWrapperArgs = (prevAttrs.makeWrapperArgs or [ ]) ++ [
+        "--prefix PYTHONPATH : ${depsPath}"
+        "--prefix PYTHONPATH : $out/${python.sitePackages}"
+      ];
+    });
+}

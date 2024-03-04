73f5c626db9f9af0be12d091702b4a655bfd1be7
Benedikt Rips <benedikt.rips@gmail.com>
2024-03-04 10:07:02 +0100

    feat(nvim): add the hardtime plugin

---
+++ .config/nvim/lua/config/plugins/hardtime.lua
@@ -0,0 +1,33 @@
+return {
+  'm4xshen/hardtime.nvim',
+  dependencies = {
+    'MunifTanjim/nui.nvim',
+    'nvim-lua/plenary.nvim',
+  },
+  opts = {
+    disable_mouse = false,
+    resetting_keys = {
+      ['[ '] = { 'n' },
+      ['[e'] = { 'n' },
+      ['] '] = { 'n' },
+      [']e'] = { 'n' },
+      -- ['g<C-J>'] = { 'n' },
+      ['ga'] = { 'n' },
+      ['gb'] = { 'n' },
+      ['gc'] = { 'n' },
+      ['gl'] = { 'n' },
+      ['gL'] = { 'n' },
+      ['go'] = { 'n' },
+    },
+    restricted_keys = {
+      ['h'] = {},
+      ['j'] = {},
+      ['k'] = {},
+      ['l'] = {},
+      ['<C-M>'] = {},
+      ['<C-N>'] = {},
+      ['<C-P>'] = {},
+      ['<CR>'] = {},
+    },
+  },
+}

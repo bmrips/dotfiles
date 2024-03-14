39754284b969f97b6983cd0798ec7c35d25a2566
Benedikt Rips <benedikt.rips@gmail.com>
2024-03-14 21:08:37 +0100

    fix(sh): do not set BROWSER

--- .config/sh/profile.sh
+++ .config/sh/profile.sh
@@ -5,6 +5,4 @@ prepend_to_path PATH "$HOME/.local/bin"
 # Add Nix' share directory
 prepend_to_path XDG_DATA_DIRS "$XDG_STATE_HOME/nix/profile/share"
 
-export BROWSER=firefox
-
 load_plugins "$HOME/.config/sh/profile.d"

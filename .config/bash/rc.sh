712f9d6a4e37cd23e85f75d7939996d7e2c918c6
Benedikt Rips <benedikt.rips@gmail.com>
2024-05-04 13:17:51 +0200

    chore(bash): remove unused colors

--- .config/bash/rc.sh
+++ .config/bash/rc.sh
@@ -16,17 +16,6 @@ shopt -s globstar
 
 mkdir -p "$XDG_CACHE_HOME/bash" "$XDG_STATE_HOME/bash"
 
-# Colors
-BLACK="$(tput setaf 0)"
-RED="$(tput setaf 1)"
-GREEN="$(tput setaf 2)"
-YELLOW="$(tput setaf 3)"
-BLUE="$(tput setaf 4)"
-MAGENTA="$(tput setaf 5)"
-CYAN="$(tput setaf 6)"
-WHITE="$(tput setaf 7)"
-RESET="$(tput sgr0)"
-
 # History
 HISTSIZE=10000
 HISTFILESIZE=$HISTSIZE

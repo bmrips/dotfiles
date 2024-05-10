cc27eb2487288fb40e6e0b42db713370f8bd9f3d
Benedikt Rips <benedikt.rips@gmail.com>
2024-05-10 21:13:08 +0200

    feat(nvim): in Nix, recognise symbols with dashes

--- .config/nvim/after/ftplugin/nix.lua
+++ .config/nvim/after/ftplugin/nix.lua
@@ -1,6 +1,7 @@
 local opt = vim.opt_local
 
+opt.iskeyword:append '-'
 opt.suffixesadd = { '.nix', '/default.nix' }
 opt.tabstop = 2
 
-vim.b.undo_ftplugin = 'set suffixesadd< tabstop<'
+vim.b.undo_ftplugin = 'set iskeyword< suffixesadd< tabstop<'

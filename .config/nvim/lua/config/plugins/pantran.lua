3671cd0aebd32168bb8719e3dc6e76b2bb56516f
Benedikt Rips <benedikt.rips@gmail.com>
2024-05-16 11:12:25 +0200

    feat(nvim): DeepL as default translation engine

--- .config/nvim/lua/config/plugins/pantran.lua
+++ .config/nvim/lua/config/plugins/pantran.lua
@@ -1,6 +1,12 @@
 return {
   'potamides/pantran.nvim',
   opts = {
-    default_engine = 'google',
+    default_engine = 'deepl',
+    engines = {
+      deepl = {
+        default_target = 'EN-GB',
+        auth_key = 'c6924529-199a-403a-8183-7293be0725d7:fx',
+      },
+    },
   },
 }

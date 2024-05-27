d19d7650d37eccd4e31ccae14152a983c5d5bf8f
Benedikt Rips <benedikt.rips@gmail.com>
2024-05-27 11:48:49 +0200

    feat(nvim): enable Lua language server addons

--- .config/nvim/lua/config/plugins/lspconfig.lua
+++ .config/nvim/lua/config/plugins/lspconfig.lua
@@ -61,7 +61,6 @@ local servers = {
       },
       -- Make the server aware of Neovim runtime files
       workspace = {
-        checkThirdParty = false,
         library = {
           vim.api.nvim_get_runtime_file('', true),
         },

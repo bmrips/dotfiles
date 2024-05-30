cc1f40e4a633789013950a69d638ee2c1e474224
Benedikt Rips <benedikt.rips@gmail.com>
2024-05-30 21:54:02 +0200

    feat(nvim): check buffer-specific LSP apabilities

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -188,13 +188,17 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
     local client = vim.lsp.get_client_by_id(args.data.client_id)
 
-    if client.supports_method 'inlayHintProvider' then
+    local function client_supports(cap)
+      client.supports_method(cap, { bufnr = args.buf })
+    end
+
+    if client_supports 'inlayHintProvider' then
       vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
     end
 
     appliedMappings.lsp[args.buf] = nest.applyKeymaps {
       buffer = args.buf,
-      mappings.lsp(client.supports_method),
+      mappings.lsp(client_supports),
     }
   end,
 })

c3e8f3be38c2a05027f445bd91fa1d16cf034d87
Benedikt Rips <benedikt.rips@gmail.com>
2024-06-08 16:49:20 +0200

    feat(nvim): add plugin's `lua/` dirs to path

--- .config/nvim/after/ftplugin/lua.lua
+++ .config/nvim/after/ftplugin/lua.lua
@@ -6,5 +6,9 @@ opt.path:append 'lua/'
 opt.suffixesadd = { '.lua', '/init.lua' }
 opt.tabstop = 2
 
+for _, dir in ipairs(vim.api.nvim_get_runtime_file('lua', true)) do
+  opt.path:append(dir)
+end
+
 vim.b.undo_ftplugin = vim.b.undo_ftplugin
   .. '| set includeexpr< omnifunc< path< suffixesadd< tabstop<'

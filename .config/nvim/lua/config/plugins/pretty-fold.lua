3747c402a549d20fae7165f3e78b84033181e4ba
Benedikt Rips <benedikt.rips@gmail.com>
2024-06-20 11:24:52 +0200

    fix(nvim): use fork of pretty-fold

--- .config/nvim/lua/config/plugins/pretty-fold.lua
+++ .config/nvim/lua/config/plugins/pretty-fold.lua
@@ -1,5 +1,7 @@
 return {
-  'anuvyklack/pretty-fold.nvim',
+  -- use fork of 'anuvyklack/pretty-fold.nvim' until
+  -- https://github.com/anuvyklack/pretty-fold.nvim/issues/39 is merged
+  'bbjornstad/pretty-fold.nvim',
   opts = {
     fill_char = ' ',
   },

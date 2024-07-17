3d82541f14be5f76621fff8d625efacba091b14d
Benedikt Rips <benedikt.rips@gmail.com>
2024-07-17 17:57:21 +0200

    fix(hm): separate option and value with `=`

--- .config/home-manager/overlays/gnu_option_constructors.nix
+++ .config/home-manager/overlays/gnu_option_constructors.nix
@@ -1,7 +1,8 @@
 final: prev:
 
 let
-  gnuCommandArgs = final.lib.cli.toGNUCommandLine { };
+  gnuCommandArgs =
+    final.lib.cli.toGNUCommandLine { optionValueSeparator = "="; };
   gnuCommandLine = attrs: final.lib.concatStringsSep " " (gnuCommandArgs attrs);
 
 in { lib = prev.lib // { inherit gnuCommandArgs gnuCommandLine; }; }

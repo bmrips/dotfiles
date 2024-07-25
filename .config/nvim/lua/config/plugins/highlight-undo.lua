cdf486ca5d9acc7b33fed2293af0058116257b1d
Benedikt Rips <benedikt.rips@gmail.com>
2024-07-25 06:42:06 +0200

    fix(nvim): link HighlightRedo to Visual as well

--- .config/nvim/lua/config/plugins/highlight-undo.lua
+++ .config/nvim/lua/config/plugins/highlight-undo.lua
@@ -5,6 +5,7 @@ return {
     require('highlight-undo').setup()
 
     local setHlGroup = function()
+      vim.api.nvim_set_hl(0, 'HighlightRedo', { link = 'Visual' })
       vim.api.nvim_set_hl(0, 'HighlightUndo', { link = 'Visual' })
     end
 

66a83beb60dd41f879f35e5f3e6d9ce93d4dcd57
Benedikt Rips <benedikt.rips@gmail.com>
2024-07-26 21:25:09 +0200

    feat(konsole): depend font size on screen size

--- .config/home-manager/profiles/kde-plasma.nix
+++ .config/home-manager/profiles/kde-plasma.nix
@@ -2,10 +2,28 @@
 
 with lib;
 
-{
+let
+  smartly-sized-konsole = pkgs.writeShellApplication {
+    name = "smartly-sized-konsole";
+    runtimeInputs = with pkgs; [ jq kdePackages.konsole kdePackages.kscreen ];
+    text = ''
+      background=''${1-Dark}
+      screen_width="$(kscreen-console json | jq .screen.currentSize.width)"
+
+      if (( screen_width == 1920 )); then
+          konsole --profile "$background"
+      else
+          konsole --profile "$background-11pt"
+      fi
+    '';
+  };
+
+in {
   options.profiles.kde-plasma.enable = mkEnableOption "the KDE Plasma profile";
 
   config = mkIf config.profiles.kde-plasma.enable {
+
+    home.packages = [ smartly-sized-konsole ];
 
     programs.firefox = {
       nativeMessagingHosts = [ pkgs.kdePackages.plasma-browser-integration ];

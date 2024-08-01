3374fe123ebc4ae9bfdba69455e9f405bb359d78
Benedikt Rips <benedikt.rips@gmail.com>
2024-08-01 17:24:21 +0200

    fix(zsh): move the surround mappings to `[ycd]z`

--- .config/home-manager/programs/zsh/vimMode.nix
+++ .config/home-manager/programs/zsh/vimMode.nix
@@ -46,10 +46,10 @@ with lib;
       zle -N delete-surround surround
       zle -N add-surround surround
       zle -N change-surround surround
-      bindkey -a 'cs' change-surround
-      bindkey -a 'ds' delete-surround
-      bindkey -a 'ys' add-surround
-      bindkey -M visual 'S' add-surround
+      bindkey -a 'cz' change-surround
+      bindkey -a 'dz' delete-surround
+      bindkey -a 'yz' add-surround
+      bindkey -M visual 'Z' add-surround
     '';
 
   };

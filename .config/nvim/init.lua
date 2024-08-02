af02c2dcf31d4b4a09fcdc6636b4486270e494d3
Benedikt Rips <benedikt.rips@gmail.com>
2024-08-02 10:14:19 +0200

    feat(nvim): synchronize background with terminal

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -45,6 +45,24 @@ opt.wildmode = { 'longest', 'full' } -- Complete till longest common string
 vim.g.mapleader = vim.keycode '<C-\\>'
 vim.g.maplocalleader = '\\'
 
+-- synchronize the background color with the terminal to remove the padding
+vim.api.nvim_create_autocmd({ 'UIEnter', 'ColorScheme' }, {
+  desc = 'Synchronize the background color with the terminal',
+  callback = function()
+    local normal = vim.api.nvim_get_hl(0, { name = 'Normal' })
+    if not normal.bg then
+      return
+    end
+    io.write(string.format('\027]11;#%06x\027\\', normal.bg))
+  end,
+})
+vim.api.nvim_create_autocmd('UILeave', {
+  desc = 'Stop synchronizing the background color with the terminal',
+  callback = function()
+    io.write '\027]111\027\\'
+  end,
+})
+
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {

9552ba2dfeb4401872290d9407e900a68f639e07
Benedikt Rips <benedikt.rips@gmail.com>
2024-08-23 14:21:39 +0200

    chore: remove previous home-manager configuration

--- .config/nix/channels/nixpkgs.nix
+++
@@ -1,8 +0,0 @@
-let
-  rev = "442d407992384ed9c0e6d352de75b69079904e4e";
-  sha256 = "0rbaxymznpr2gfl5a9jyii5nlpjc9k2lrwlw2h5ccinds58c202k";
-in import (fetchTarball {
-  name = "nixpkgs";
-  url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-  inherit sha256;
-})

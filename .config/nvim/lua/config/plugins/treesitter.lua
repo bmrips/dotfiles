fa6ad7bda832e27f609db399b0be2ceddf6ea1fa
Benedikt Rips <benedikt.rips@gmail.com>
2024-08-28 14:52:17 +0200

    fix(nvim): remove treesitter incremental selection

--- .config/nvim/lua/config/plugins/treesitter.lua
+++ .config/nvim/lua/config/plugins/treesitter.lua
@@ -4,15 +4,6 @@ return {
   build = ':TSUpdate',
   opts = {
     highlight = { enable = true },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = 'gyi',
-        node_decremental = 'gy[',
-        node_incremental = 'gy]',
-        scope_incremental = 'gys',
-      },
-    },
     indent = { enable = true },
     matchup = { enable = true },
 

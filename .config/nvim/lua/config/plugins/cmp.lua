acb15ae744605c12517a0957d950438774ee73d5
Benedikt Rips <benedikt.rips@gmail.com>
2024-08-28 13:28:09 +0200

    chore(nvim): remove superfluous configuration/code

--- .config/nvim/lua/config/plugins/cmp.lua
+++ .config/nvim/lua/config/plugins/cmp.lua
@@ -43,10 +43,9 @@ return {
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm { select = false },
       },
-      sources = cmp.config.sources {
+      sources = {
         { name = 'nvim_lsp' },
         { name = 'nvim_lsp_signature_help' },
         { name = 'luasnip' },

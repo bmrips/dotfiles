ac477ef3678c4a27e3789d52ee04461570d1e81e
Benedikt Rips <benedikt.rips@gmail.com>
2024-10-22 14:41:29 +0200

    refactor(hm): pre-commit depends on dev profiles

--- .config/home-manager/home-manager/config/ci/pre-commit.nix
+++ .config/home-manager/home-manager/config/ci/pre-commit.nix
@@ -6,6 +6,8 @@ with lib;
   options.ci.pre-commit.enable = mkEnableOption "pre-commit integration";
 
   config = mkIf config.ci.pre-commit.enable {
-    home.packages = (with pkgs; [ gitlint nixfmt-classic pre-commit yamlfmt ]);
+    home.packages = (with pkgs; [ gitlint pre-commit ]);
+    development.nix.enable = true;
+    development.yaml.enable = true;
   };
 }

ea85957b82aa53cbfc7f57dae570889cf6622916
Benedikt Rips <benedikt.rips@gmail.com>
2024-10-22 14:45:21 +0200

    ci(pre-commit): run mdformat and markdownlint

--- .config/home-manager/home-manager/config/ci/pre-commit.nix
+++ .config/home-manager/home-manager/config/ci/pre-commit.nix
@@ -7,6 +7,7 @@ with lib;
 
   config = mkIf config.ci.pre-commit.enable {
     home.packages = (with pkgs; [ gitlint pre-commit ]);
+    development.markdown.enable = true;
     development.nix.enable = true;
     development.lua.enable = true;
     development.yaml.enable = true;

f33546b0f398cbd312eb34fd7ef48d048a62ba1b
Benedikt Rips <benedikt.rips@gmail.com>
2024-11-27 23:34:52 +0100

    fix(nixpkgs): do not warn of undeclared options

--- .config/home-manager/nixpkgs/default.nix
+++ .config/home-manager/nixpkgs/default.nix
@@ -4,7 +4,6 @@
     allowAliases = false;
     allowUnfree = true;
     permittedInsecurePackages = [ "electron-27.3.11" ];
-    warnUndeclaredOptions = true;
   };
 
   nixpkgs.overlays = map import [ ./packages ./pre-commit_with_meta_hooks.nix ];

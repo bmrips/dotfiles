d2c2cf71c7cadef5549d73efbe61a29a1dd2a038
Benedikt Rips <benedikt.rips@gmail.com>
2024-12-09 15:13:59 +0100

    feat(slack): open centered on the screen

--- .config/home-manager/home-manager/config/programs/slack.nix
+++ .config/home-manager/home-manager/config/programs/slack.nix
@@ -1,1 +1,19 @@
-{ programs.slack.autostart = true; }
+{ config, lib, ... }:
+
+let inherit (lib) mkIf;
+
+in {
+  programs.slack.autostart = true;
+
+  programs.plasma.window-rules = mkIf config.programs.slack.enable [{
+    description = "Slack";
+    match = {
+      window-class.value = "slack Slack";
+      window-role.value = "browser-window";
+    };
+    apply = {
+      placement.apply = "force";
+      placement.value = 5;
+    };
+  }];
+}

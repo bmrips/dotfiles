19e2ff5cab90a4d2559fcb21e26045167b3cc39b
Benedikt Rips <benedikt.rips@gmail.com>
2024-12-13 13:10:30 +0100

    fix(okular): only configure if enabled

--- .config/home-manager/home-manager/config/programs/okular.nix
+++ .config/home-manager/home-manager/config/programs/okular.nix
@@ -1,7 +1,9 @@
+{ config, lib, ... }:
+
 {
   programs.okular.general.obeyDrm = false;
 
-  programs.plasma.configFile = {
+  programs.plasma.configFile = lib.mkIf config.programs.okular.enable {
     okularrc."Notification Messages".presentationInfo = false;
     okularpartrc = {
       "Dlg Presentation".SlidesShowProgress = false;

7a9c64d964fd0e1c9a905802c56921fad5f04394
Benedikt Rips <benedikt.rips@gmail.com>
2024-12-13 13:01:34 +0100

    feat(dolphin): add option for view properties

--- .config/home-manager/home-manager/config/programs/dolphin.nix
+++ .config/home-manager/home-manager/config/programs/dolphin.nix
@@ -7,74 +7,83 @@ let
 in mkMerge [
 
   {
-    programs.dolphin.shortcutSchemes.Custom = {
-      edit_undo = "Ctrl+Z";
-      edit_cut = "Ctrl+X";
-      edit_copy = "Ctrl+C";
-      edit_paste = "Ctrl+V";
-      toggle_selection_mode = "Space";
-      edit_select_all = "Ctrl+A";
-      invert_selection = "Ctrl+Shift+A";
-      create_dir = "Ctrl+N";
-      renamefile = "Ctrl+R";
-      duplicate = "Ctrl+D";
-      movetotrash = "Del";
-      deletefile = "Shift+Del";
-      properties = [ "Alt+Return" "Alt+Enter" ];
-      go_back = [ "Ctrl+H" "Backspace" ];
-      go_forward = "Ctrl+L";
-      go_up = "Ctrl+K";
-      go_home = "Home Page";
-      copy_location = "Ctrl+Shift+L";
-      editable_location = "F6";
-      replace_location = "Ctrl+G";
-      delete_shortcut = "Del";
-      add_bookmark = "Ctrl+B";
-      split_view_menu = [ "Ctrl+S" "F3" ];
-      popout_split_view = "Shift+F3";
-      copy_to_inactive_split_view = "Ctrl+Shift+C";
-      move_to_inactive_split_view = "Ctrl+Shift+X";
-      view_redisplay = [ "F5" "Refresh" ];
-      show_filter_bar = [ "Ctrl+I" "/" ];
-      edit_find = "Ctrl+F";
-      open_preferred_search_tool = "Ctrl+Shift+F";
-      file_quit = "Ctrl+Q";
-      file_close = "Ctrl+W";
-      undo_close_tab = "Ctrl+Shift+T";
-      new_tab = "Ctrl+T";
-      activate_tab_0 = "Alt+1";
-      activate_tab_1 = "Alt+2";
-      activate_tab_2 = "Alt+3";
-      activate_tab_3 = "Alt+4";
-      activate_tab_4 = "Alt+5";
-      activate_tab_5 = "Alt+6";
-      activate_tab_6 = "Alt+7";
-      activate_tab_7 = "Alt+8";
-      activate_last_tab = "Alt+9";
-      activate_next_tab = [ "Ctrl+]" "Ctrl+Tab" ];
-      activate_prev_tab = [ "Ctrl+[" "Ctrl+Shift+Tab" ];
-      icons = "Ctrl+1";
-      compact = "Ctrl+2";
-      details = "Ctrl+3";
-      view_zoom_in = [ "Ctrl++" "Ctrl+=" ];
-      view_zoom_out = "Ctrl+-";
-      view_zoom_reset = "Ctrl+0";
-      show_preview = "Ctrl+P";
-      show_hidden_files = [ "Ctrl+." "." ];
-      help_contents = "F1";
-      help_whats_this = "Shift+F1";
-      show_terminal_panel = "F4";
-      open_terminal = "Shift+F4";
-      open_terminal_here = "Alt+Shift+F4";
-      focus_terminal_panel = "Ctrl+Shift+F4";
-      show_folders_panel = "F7";
-      show_places_panel = "F9";
-      hamburger_menu = "F10";
-      show_information_panel = "F11";
-      options_show_menubar = "Ctrl+M";
-      options_configure_keybinding = "Ctrl+>";
-      options_configure = "Ctrl+Shift+,";
-      open_kcommand_bar = "Ctrl+Alt+I";
+    programs.dolphin = {
+
+      shortcutSchemes.Custom = {
+        edit_undo = "Ctrl+Z";
+        edit_cut = "Ctrl+X";
+        edit_copy = "Ctrl+C";
+        edit_paste = "Ctrl+V";
+        toggle_selection_mode = "Space";
+        edit_select_all = "Ctrl+A";
+        invert_selection = "Ctrl+Shift+A";
+        create_dir = "Ctrl+N";
+        renamefile = "Ctrl+R";
+        duplicate = "Ctrl+D";
+        movetotrash = "Del";
+        deletefile = "Shift+Del";
+        properties = [ "Alt+Return" "Alt+Enter" ];
+        go_back = [ "Ctrl+H" "Backspace" ];
+        go_forward = "Ctrl+L";
+        go_up = "Ctrl+K";
+        go_home = "Home Page";
+        copy_location = "Ctrl+Shift+L";
+        editable_location = "F6";
+        replace_location = "Ctrl+G";
+        delete_shortcut = "Del";
+        add_bookmark = "Ctrl+B";
+        split_view_menu = [ "Ctrl+S" "F3" ];
+        popout_split_view = "Shift+F3";
+        copy_to_inactive_split_view = "Ctrl+Shift+C";
+        move_to_inactive_split_view = "Ctrl+Shift+X";
+        view_redisplay = [ "F5" "Refresh" ];
+        show_filter_bar = [ "Ctrl+I" "/" ];
+        edit_find = "Ctrl+F";
+        open_preferred_search_tool = "Ctrl+Shift+F";
+        file_quit = "Ctrl+Q";
+        file_close = "Ctrl+W";
+        undo_close_tab = "Ctrl+Shift+T";
+        new_tab = "Ctrl+T";
+        activate_tab_0 = "Alt+1";
+        activate_tab_1 = "Alt+2";
+        activate_tab_2 = "Alt+3";
+        activate_tab_3 = "Alt+4";
+        activate_tab_4 = "Alt+5";
+        activate_tab_5 = "Alt+6";
+        activate_tab_6 = "Alt+7";
+        activate_tab_7 = "Alt+8";
+        activate_last_tab = "Alt+9";
+        activate_next_tab = [ "Ctrl+]" "Ctrl+Tab" ];
+        activate_prev_tab = [ "Ctrl+[" "Ctrl+Shift+Tab" ];
+        icons = "Ctrl+1";
+        compact = "Ctrl+2";
+        details = "Ctrl+3";
+        view_zoom_in = [ "Ctrl++" "Ctrl+=" ];
+        view_zoom_out = "Ctrl+-";
+        view_zoom_reset = "Ctrl+0";
+        show_preview = "Ctrl+P";
+        show_hidden_files = [ "Ctrl+." "." ];
+        help_contents = "F1";
+        help_whats_this = "Shift+F1";
+        show_terminal_panel = "F4";
+        open_terminal = "Shift+F4";
+        open_terminal_here = "Alt+Shift+F4";
+        focus_terminal_panel = "Ctrl+Shift+F4";
+        show_folders_panel = "F7";
+        show_places_panel = "F9";
+        hamburger_menu = "F10";
+        show_information_panel = "F11";
+        options_show_menubar = "Ctrl+M";
+        options_configure_keybinding = "Ctrl+>";
+        options_configure = "Ctrl+Shift+,";
+        open_kcommand_bar = "Ctrl+Alt+I";
+      };
+
+      viewProperties.global = {
+        PreviewsShown = false;
+        ViewMode = 1;
+      };
+
     };
   }
 
@@ -115,12 +124,6 @@ in mkMerge [
       PlacesPanel.IconSize = 16;
       "Shortcut Schemes"."Current Scheme" = "Custom";
     };
-
-    programs.plasma.dataFile."dolphin/view_properties/global/.directory".Dolphin =
-      {
-        "PreviewsShown" = false;
-        "ViewMode" = 1;
-      };
 
     # Set in Dolphin's settings.
     programs.plasma.configFile.kiorc = {
--- .config/home-manager/home-manager/modules/programs/dolphin.nix
+++ .config/home-manager/home-manager/modules/programs/dolphin.nix
@@ -1,17 +1,58 @@
 { config, lib, ... }:
 
 let
-  inherit (lib) mkEnableOption mkIf plasma;
+  inherit (lib) mapAttrs' mkEnableOption mkIf mkOption mkMerge plasma types;
+
   cfg = config.programs.dolphin;
+
+  basicSettingsType = with types; nullOr (oneOf [ bool float int str ]);
+  viewPropertiesType = types.attrsOf basicSettingsType;
+  viewPropertiesOption = kind:
+    mkOption {
+      type = viewPropertiesType;
+      default = { };
+      description = "${kind} view properties.";
+      example = {
+        PreviewsShown = false;
+        ViewMode = 1;
+      };
+    };
 
 in {
 
   options.programs.dolphin = {
     enable = mkEnableOption "Dolphin.";
     shortcutSchemes = plasma.shortcutSchemesOption;
+    viewProperties = {
+      global = viewPropertiesOption "Global";
+      remote = viewPropertiesOption "Remote";
+      trash = viewPropertiesOption "Trash";
+      local = mkOption {
+        type = types.attrsOf viewPropertiesType;
+        default = { };
+        description = "Directory-local view properties";
+      };
+    };
   };
 
-  config.programs.plasma.shortcutSchemes.dolphin =
-    mkIf cfg.enable cfg.shortcutSchemes;
+  config.programs.plasma = {
+
+    shortcutSchemes.dolphin = mkIf cfg.enable cfg.shortcutSchemes;
+
+    dataFile =
+      let mkFilename = kind: "dolphin/view_properties/${kind}/.directory";
+      in mkMerge [
+        {
+          ${mkFilename "global"}.Dolphin = cfg.viewProperties.global;
+          ${mkFilename "remote"}.Dolphin = cfg.viewProperties.remote;
+          ${mkFilename "trash"}.Dolphin = cfg.viewProperties.remote;
+        }
+        (mapAttrs' (path: props: {
+          name = mkFilename "local/${path}";
+          value.Dolphin = props;
+        }) cfg.viewProperties.local)
+      ];
+
+  };
 
 }

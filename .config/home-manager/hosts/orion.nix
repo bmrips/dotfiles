a0110f3087200ddf2922f4dc56d5a505c82a1e9e
Benedikt Rips <benedikt.rips@gmail.com>
2025-01-13 21:27:37 +0100

    chore(nixos): disable Nextcloud on orion
    I'm no longer part of RVLH's youth board.

--- .config/home-manager/hosts/orion.nix
+++ .config/home-manager/hosts/orion.nix
@@ -186,8 +186,6 @@ in
       }
     ];
 
-    services.nextcloud-client.enable = true;
-
     # This value determines the Home Manager release that your configuration is
     # compatible with. This helps avoid breakage when a new Home Manager release
     # introduces backwards incompatible changes.

48a0f3b404f8c1a6b8a943e9472bf53de5080bfd
Benedikt Rips <benedikt.rips@gmail.com>
2025-03-03 09:36:51 +0100

    fix(pre-commit): do not propagage the Python env

--- .config/home-manager/nixpkgs/pre-commit_with_meta_hooks.nix
+++ .config/home-manager/nixpkgs/pre-commit_with_meta_hooks.nix
@@ -8,6 +8,10 @@ final: prev:
       depsPath = makePythonPath prevAttrs.propagatedBuildInputs;
     in
     {
+      # Propagating dependencies leaks them through $PYTHONPATH which causes issues
+      # when used in nix-shell. Instead, set $PYTHONPATH only for pre-commit by
+      # creating a wrapper.
+      postFixup = "rm $out/nix-support/propagated-build-inputs";
       makeWrapperArgs = (prevAttrs.makeWrapperArgs or [ ]) ++ [
         "--prefix PYTHONPATH : ${depsPath}"
         "--prefix PYTHONPATH : $out/${python.sitePackages}"

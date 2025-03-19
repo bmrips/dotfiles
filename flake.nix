fed58e08ae27b38ca6ced3106c89c3b8be16bc23
Benedikt Rips <benedikt.rips@gmail.com>
2025-03-19 21:13:20 +0100

    refactor: use flake-parts

--- flake.nix
+++ flake.nix
@@ -1,8 +1,9 @@
 {
 
-  description = "NixOS configuration";
+  description = "My dotfiles";
 
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -23,7 +24,7 @@
       inputs.home-manager.follows = "home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    pre-commit-hooks = {
+    pre-commit = {
       url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
@@ -34,117 +35,117 @@
   };
 
   outputs =
-    {
-      self,
-      home-manager,
-      nixos-hardware,
-      nixpkgs,
-      nixpkgs_23_05,
-      nix-index-database,
-      nur,
-      pre-commit-hooks,
-      plasma-manager,
-      programs-db,
-      ...
-    }@inputs:
-    let
-      lib = nixpkgs.lib.extend (import ./lib);
-      user = "bmr";
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      pkgs_23_05 = import nixpkgs_23_05 { inherit system; };
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      { withSystem, ... }:
+      {
 
-      mkNixosConfig =
-        {
-          host,
-          extraModules ? [ ],
-        }:
-        nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs = {
-            inherit
-              host
-              inputs
-              lib
-              user
-              ;
+        imports = [ inputs.pre-commit.flakeModule ];
+
+        systems = [ "x86_64-linux" ];
+
+        flake.nixosConfigurations =
+          let
+            lib = inputs.nixpkgs.lib.extend (import ./lib);
+
+            mkNixosConfig =
+              {
+                system ? "x86_64-linux",
+                host,
+                extraModules ? [ ],
+              }:
+              withSystem system (
+                { inputs', ... }:
+                lib.nixosSystem {
+                  inherit system;
+                  specialArgs = {
+                    user = "bmr";
+                    inherit
+                      host
+                      inputs
+                      lib
+                      ;
+                  };
+                  modules = [
+                    ./nixos
+                    inputs.nix-index-database.nixosModules.nix-index
+                    inputs.nur.modules.nixos.default
+                    inputs.home-manager.nixosModules.home-manager
+                    {
+                      home-manager = {
+                        useGlobalPkgs = true;
+                        extraSpecialArgs = {
+                          pkgs_23_05 = inputs'.nixpkgs_23_05.legacyPackages;
+                          programs-db = inputs'.programs-db.packages.programs-sqlite;
+                        };
+                        sharedModules = [
+                          ./home-manager
+                          inputs.plasma-manager.homeManagerModules.plasma-manager
+                        ];
+                      };
+                    }
+                  ] ++ extraModules;
+                }
+              );
+          in
+          {
+            orion = mkNixosConfig {
+              host = "orion";
+              extraModules = [ ./hosts/orion.nix ];
+            };
+            radboud = mkNixosConfig {
+              host = "radboud";
+              extraModules = [ ./hosts/radboud.nix ];
+            };
           };
-          modules = [
-            ./nixos
-            nix-index-database.nixosModules.nix-index
-            nur.modules.nixos.default
-            home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                extraSpecialArgs = {
-                  inherit pkgs_23_05;
-                  programs-db = programs-db.packages.${system}.programs-sqlite;
-                };
-                sharedModules = [
-                  ./home-manager
-                  plasma-manager.homeManagerModules.plasma-manager
-                ];
+
+        perSystem =
+          {
+            config,
+            inputs',
+            pkgs,
+            ...
+          }:
+          {
+            devShells.default = config.pre-commit.devShell;
+            pre-commit.settings.hooks =
+              let
+                mdformat = pkgs.mdformat.withPlugins (
+                  ps: with ps; [
+                    mdformat-footnote
+                    mdformat-gfm
+                    mdformat-gfm-alerts
+                    mdformat-tables
+                  ]
+                );
+              in
+              {
+                check-added-large-files.enable = true;
+                check-executables-have-shebangs.enable = true;
+                check-merge-conflicts.enable = true;
+                check-shebang-scripts-are-executable.enable = true;
+                check-symlinks.enable = true;
+                check-toml.enable = true;
+                check-vcs-permalinks.enable = true;
+                check-yaml.enable = true;
+                convco.enable = true;
+                detect-private-keys.enable = true;
+                markdownlint.enable = true;
+                mdformat.enable = true;
+                mdformat.package = mdformat;
+                mixed-line-endings.enable = true;
+                nixfmt-rfc-style.enable = true;
+                selene.args = [ "--no-summary" ];
+                selene.enable = true;
+                shellcheck.enable = true;
+                shfmt.args = [ "--indent=4" ];
+                shfmt.enable = true;
+                stylua.enable = true;
+                trim-trailing-whitespace.enable = true;
+                typos.enable = true;
               };
-            }
-          ] ++ extraModules;
-        };
+          };
 
-      mdformat = pkgs.mdformat.withPlugins (
-        ps: with ps; [
-          mdformat-footnote
-          mdformat-gfm
-          mdformat-gfm-alerts
-          mdformat-tables
-        ]
-      );
-
-      pre-commit-check = pre-commit-hooks.lib.${system}.run {
-        src = ./.;
-        hooks = {
-          check-added-large-files.enable = true;
-          check-executables-have-shebangs.enable = true;
-          check-merge-conflicts.enable = true;
-          check-shebang-scripts-are-executable.enable = true;
-          check-symlinks.enable = true;
-          check-toml.enable = true;
-          check-vcs-permalinks.enable = true;
-          check-yaml.enable = true;
-          convco.enable = true;
-          detect-private-keys.enable = true;
-          markdownlint.enable = true;
-          mdformat.enable = true;
-          mdformat.package = mdformat;
-          mixed-line-endings.enable = true;
-          nixfmt-rfc-style.enable = true;
-          shellcheck.enable = true;
-          shfmt.args = [ "--indent=4" ];
-          shfmt.enable = true;
-          stylua.enable = true;
-          trim-trailing-whitespace.enable = true;
-          typos.enable = true;
-        };
-      };
-
-    in
-    {
-      nixosConfigurations = {
-        orion = mkNixosConfig {
-          host = "orion";
-          extraModules = [ ./hosts/orion.nix ];
-        };
-        radboud = mkNixosConfig {
-          host = "radboud";
-          extraModules = [ ./hosts/radboud.nix ];
-        };
-      };
-
-      checks.${system}.pre-commit = pre-commit-check;
-
-      devShells.${system}.default = pkgs.mkShell {
-        packages = pre-commit-check.enabledPackages;
-        inherit (pre-commit-check) shellHook;
-      };
-    };
-
+      }
+    );
 }

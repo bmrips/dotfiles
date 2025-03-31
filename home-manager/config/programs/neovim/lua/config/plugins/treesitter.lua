1545c0a00ef3caab56a2d046e0dd9782932cdbb9
Benedikt Rips <benedikt.rips@gmail.com>
2025-03-31 16:16:13 +0200

    perf(nvim): bundle treesitter integrations
    Also, the treesitter integrations are only loaded when treesitter itself
    is loaded.

--- home-manager/config/programs/neovim/lua/config/plugins/treesitter.lua
+++ home-manager/config/programs/neovim/lua/config/plugins/treesitter.lua
@@ -1,20 +1,37 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  event = 'FileType',
-  build = ':TSUpdate',
-  opts = {
-    highlight = { enable = true },
-    indent = { enable = true },
-    matchup = { enable = true },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    event = 'FileType',
+    build = ':TSUpdate',
+    opts = {
+      highlight = { enable = true },
+      indent = { enable = true },
+      matchup = { enable = true },
 
-    ensure_installed = 'all',
+      ensure_installed = 'all',
 
-    -- Automatically install missing parsers when entering buffer.
-    auto_install = false,
+      -- Automatically install missing parsers when entering buffer.
+      auto_install = false,
+    },
+    config = function(_, opts)
+      require('nvim-treesitter.configs').setup(opts)
+      vim.opt.foldmethod = 'expr'
+      vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    end,
   },
-  config = function(_, opts)
-    require('nvim-treesitter.configs').setup(opts)
-    vim.opt.foldmethod = 'expr'
-    vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-  end,
+  {
+    'nvim-treesitter/nvim-treesitter',
+    optional = true,
+    dependencies = 'RRethy/nvim-treesitter-endwise',
+    opts = {
+      endwise = { enable = true },
+    },
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    optional = true,
+    dependencies = {
+      { 'windwp/nvim-ts-autotag', config = true },
+    },
+  },
 }

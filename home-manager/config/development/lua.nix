db2cf23f70e9a78f5832fb30ff5db96960e24f0e
Benedikt Rips <benedikt.rips@gmail.com>
2025-04-11 17:02:08 +0200

    fix(stylua): disable --search-parent-directories

--- home-manager/config/development/lua.nix
+++ home-manager/config/development/lua.nix
@@ -9,7 +9,6 @@
   options.development.lua.enable = lib.mkEnableOption "Lua development tools.";
 
   config = lib.mkIf config.development.lua.enable {
-    home.defaultCommandFlags.stylua.search-parent-directories = true;
     home.packages = with pkgs; [
       lua-language-server
       selene

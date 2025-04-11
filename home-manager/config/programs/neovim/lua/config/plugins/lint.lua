bc4cf37777341726765d17ee520b49465e06c242
Benedikt Rips <benedikt.rips@gmail.com>
2025-04-11 17:25:46 +0200

    feat(nvim): integrate lint

--- home-manager/config/programs/neovim/lua/config/plugins/lint.lua
+++ home-manager/config/programs/neovim/lua/config/plugins/lint.lua
@@ -1,4 +1,25 @@
 return {
   'mfussenegger/nvim-lint',
-  opts = {},
+  init = function()
+    vim.api.nvim_create_autocmd(
+      { 'BufWritePost', 'BufReadPost', 'BufModifiedSet', 'InsertLeave' },
+      {
+        group = vim.api.nvim_create_augroup('nvim-lint', { clear = true }),
+        desc = 'Invoke linters',
+        callback = function()
+          require('lint').try_lint(nil, { ignore_errors = true })
+        end,
+      }
+    )
+  end,
+  config = function()
+    require('lint').linters_by_ft = {
+      editorconfig = { 'editorconfig-checker' },
+      lua = { 'selene' },
+      markdown = { 'markdownlint' },
+      nix = { 'deadnix', 'statix' },
+      yaml = { 'yamllint' },
+      zsh = { 'zsh' },
+    }
+  end,
 }

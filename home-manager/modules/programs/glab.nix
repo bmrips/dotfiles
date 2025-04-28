4ac0eb5e5e27f1c707a2c29f36c40476e0664049
Benedikt Rips <benedikt.rips@gmail.com>
2025-04-28 15:04:43 +0200

    fix(glab): apply settings side-effectfully

--- home-manager/modules/programs/glab.nix
+++ home-manager/modules/programs/glab.nix
@@ -39,13 +39,22 @@ in
 
   config = mkIf cfg.enable {
     home.packages = [ cfg.package ];
-    xdg.configFile = {
-      "glab-cli/aliases.yml" = mkIf (cfg.aliases != { }) {
-        source = yaml.generate "glab-cli_aliases.yml" cfg.aliases;
-      };
-      "glab-cli/config.yml" = mkIf (cfg.settings != { }) {
-        source = yaml.generate "glab-cli_config.yml" cfg.settings;
-      };
+
+    xdg.configFile."glab-cli/aliases.yml" = mkIf (cfg.aliases != { }) {
+      source = yaml.generate "glab-cli_aliases.yml" cfg.aliases;
     };
+
+    home.activation.glab-config =
+      let
+        configFile = "${config.xdg.configHome}/glab-cli/config.yml";
+      in
+      # The `home.file` mechanism fails in this case since glab requires its
+      # configuration file to have 0600 permissions and to be owned by the user.
+      lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        run mkdir -p '${config.xdg.configHome}/glab-cli'
+        run touch '${configFile}'
+        run ${pkgs.yq-go}/bin/yq --inplace $VERBOSE_ARG '. *= load("${yaml.generate "glab-cli_config.yml" cfg.settings}")' '${configFile}'
+        run chmod 0600 ${configFile}
+      '';
   };
 }

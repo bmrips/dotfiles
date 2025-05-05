ed948cfe0b0f679391ca0e15a91cc405658ac265
Benedikt Rips <benedikt.rips@gmail.com>
2025-05-05 06:16:31 +0200

    refactor: upstream the goto package

--- nixpkgs/packages/default.nix
+++ nixpkgs/packages/default.nix
@@ -3,6 +3,5 @@ final: _prev:
 {
   bt-dualboot = final.callPackage ./bt-dualboot.nix { };
   fzf-tab-completion = final.callPackage ./fzf-tab-completion.nix { };
-  goto = final.callPackage ./goto.nix { };
   rl_custom_function = final.callPackage ./rl_custom_function.nix { };
 }

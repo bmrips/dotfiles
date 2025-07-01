b0dd2d81e0a741a2f65b96702450c17988823011
Benedikt Rips <benedikt.rips@gmail.com>
2025-07-01 16:34:19 +0200

    feat: functions for hexadecimals and RGB values

---
+++ lib/rgb.nix
@@ -0,0 +1,20 @@
+lib:
+
+{
+  rgb = rec {
+
+    parse =
+      let
+        byte = "([0-9a-fA-F]{2})";
+      in
+      hex: map lib.hex2dec (lib.match "#${byte}${byte}${byte}" hex);
+
+    print =
+      color:
+      let
+        result = "#" + lib.concatStrings (map lib.dec2hex color);
+      in
+      if parse result != null then result else throw "Error: not an RGB color specification!";
+
+  };
+}

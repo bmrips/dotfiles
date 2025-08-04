735cef42b0dba2ee8c601b2e136ce9c7c7df8864
Benedikt Rips <benedikt.rips@gmail.com>
2025-08-04 10:01:34 +0200

    fix: sanitise the standalone profile

--- home-manager/config/profiles/standalone.nix
+++ home-manager/config/profiles/standalone.nix
@@ -5,49 +5,32 @@
   ...
 }:
 
-let
-  inherit (lib)
-    attrValues
-    mkEnableOption
-    mkIf
-    mkMerge
-    unique
-    ;
-
-in
 {
-  options.profiles.standalone.enable = mkEnableOption "the standalone profile.";
+  options.profiles.standalone.enable = lib.mkEnableOption "the standalone profile.";
 
-  config = mkIf config.profiles.standalone.enable (mkMerge [
-    (import ../../../nixpkgs)
-    {
-      assertions = [
-        {
-          assertion = !config.submoduleSupport.enable;
-          message = "This profile only available on standalone installations.";
-        }
-      ];
+  config = lib.mkIf config.profiles.standalone.enable (
+    lib.mkMerge [
+      (import ../../../nixpkgs)
+      {
+        assertions = [
+          {
+            assertion = !config.submoduleSupport.enable;
+            message = "This profile only available on standalone installations.";
+          }
+        ];
 
-      i18n.glibcLocales = pkgs.glibcLocales.override {
-        allLocales = false;
-        locales = builtins.map (l: l + "/UTF-8") (
-          [
-            "C.UTF-8"
-            "en_US.UTF-8"
-          ]
-          ++ unique (attrValues config.i18n.extraLocaleSettings)
-        );
-      };
+        i18n.glibcLocales = pkgs.glibcLocales.override {
+          allLocales = false;
+          locales = [
+            "C.UTF-8/UTF-8"
+            "en_US.UTF-8/UTF-8"
+          ];
+        };
 
-      nix.gc = {
-        automatic = true;
-        options = "--delete-older-than 30d";
-        randomizedDelaySec = "1h";
-      };
+        nix.package = pkgs.nix;
 
-      nix.package = pkgs.nix;
-
-      services.home-manager.autoUpgrade.enable = true;
-    }
-  ]);
+        services.home-manager.autoUpgrade.enable = true;
+      }
+    ]
+  );
 }

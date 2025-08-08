8aa7f7488a6cd3ee23223146448fe05da98b0a14
Benedikt Rips <benedikt.rips@gmail.com>
2025-08-08 18:34:36 +0200

    feat(plasma): install the SDDM KCM

--- nixos/config/services/desktopManager/plasma6.nix
+++ nixos/config/services/desktopManager/plasma6.nix
@@ -1,12 +1,19 @@
 {
   config,
   lib,
+  pkgs,
   user,
   ...
 }:
 
 lib.mkIf config.services.desktopManager.plasma6.enable {
-  home-manager.users.${user}.programs.plasma.enable = true;
   profiles.gui.enable = true;
   services.displayManager.sddm.enable = true;
+
+  home-manager.users.${user} = {
+    programs.plasma.enable = true;
+    home.packages = lib.mkIf config.services.displayManager.sddm.enable [
+      pkgs.kdePackages.sddm-kcm
+    ];
+  };
 }

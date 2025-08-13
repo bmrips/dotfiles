4b865d69f6042fbf94adfffde3f1c22eb308cd53
Benedikt Rips <benedikt.rips@gmail.com>
2025-08-13 19:49:25 +0200

    feat(bt-dualboot): toggle and prune backups

--- nixos/modules/services/bt-dualboot.nix
+++ nixos/modules/services/bt-dualboot.nix
@@ -18,13 +18,40 @@ let
 in
 {
 
+  meta.maintainers = [ lib.maintainers.bmrips ];
+
   options.services.bt-dualboot = {
     enable = lib.mkEnableOption "{command}`bt-dualboot`";
     package = lib.mkPackageOption pkgs "bt-dualboot" { };
     mountPoint = lib.mkOption {
-      type = lib.types.str;
-      description = "The mount point of the Windows data partition.";
+      type = with lib.types; nullOr str;
+      default = null;
       example = "/mnt";
+      description = ''
+        The mount point of the Windows data partition. By default,
+        {command}`bt-dualboot` will recognize and use mount Windows partitions
+        automatically.
+      '';
+    };
+    registryBackups = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = ''
+          Whether to backup the Windows Registry before modifying it.
+        '';
+      };
+      retentionPeriod = lib.mkOption {
+        type = with lib.types; nullOr str;
+        default = null;
+        example = "4 weeks";
+        description = ''
+          For how long the registry backups are retained.
+
+          Values have to be formatted according to the 'age' field of {manpage}`tmpfiles.d(5)`.
+          Set it to `null` to retain backups forever.
+        '';
+      };
     };
   };
 
@@ -34,15 +61,28 @@ in
 
     systemd.services.bt-dualboot = {
       description = "Copy bluetooth pairing keys to Windows before shutdown";
-      requires = [ "${escapedMountPoint}.mount" ];
-      after = [ "${escapedMountPoint}.mount" ];
       wantedBy = [ "multi-user.target" ];
+
+      # Use `RemainAfterExit` and `ExecStop` to run on shutdown
       serviceConfig = {
         Type = "oneshot";
         RemainAfterExit = true;
-        ExecStop = "${pkgs.bt-dualboot}/bin/bt-dualboot --win ${cfg.mountPoint} --backup --sync-all";
+        ExecStop =
+          "${pkgs.bt-dualboot}/bin/bt-dualboot --sync-all"
+          + (if cfg.registryBackups.enable then " --backup" else " --no-backup")
+          + lib.optionalString (cfg.mountPoint != null) " --win ${cfg.mountPoint}";
       };
+    }
+    // lib.optionalAttrs (cfg.mountPoint != null) {
+      requires = [ "${escapedMountPoint}.mount" ];
+      after = [ "${escapedMountPoint}.mount" ];
     };
+
+    systemd.tmpfiles.settings.bt-dualboot."/var/backup/bt-dualboot/".e.age =
+      let
+        backups = cfg.registryBackups;
+      in
+      lib.mkIf (backups.enable && backups.retentionPeriod != null) backups.retentionPeriod;
 
   };
 

b57c3431bbafbd0e8b8dc59340a4f19df0b68e1f
Benedikt Rips <benedikt.rips@gmail.com>
2025-08-15 14:54:30 +0200

    refactor: get the home directory from the config

--- nixos/config/default.nix
+++ nixos/config/default.nix
@@ -8,6 +8,8 @@
 }:
 
 let
+  xdgConfigHome = config.home-manager.users.${user}.xdg.configHome;
+
   kmod-params = pkgs.writeShellApplication {
     name = "kmod-params";
     runtimeInputs = with pkgs; [
@@ -145,7 +147,7 @@ in
   systemd.tmpfiles.settings.nixos = {
     "%C".v.age = "4 weeks"; # put the cache into a subvolume and clean it
     "%L".v = { }; # put the log into a subvolume
-    "/etc/nixos"."L+".argument = "/home/${user}/.config/home-manager";
+    "/etc/nixos"."L+".argument = "${xdgConfigHome}/home-manager";
     "/mnt".d = { };
   };
 

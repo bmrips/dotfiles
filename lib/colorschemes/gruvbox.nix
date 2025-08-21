4ab2a490712db31ce8173de1d6b42e181b1f91d2
Benedikt Rips <benedikt.rips@gmail.com>
2025-08-22 00:30:18 +0200

    refactor(gruvbox): include all colors

--- lib/colorschemes/gruvbox.nix
+++ lib/colorschemes/gruvbox.nix
@@ -3,78 +3,100 @@
 rec {
 
   colors = {
-    background.dark = {
-      hard = "#1d2021";
-      medium = "#282828";
-      soft = "#32302f";
-    };
-    background.light = {
-      hard = "#f9f5d7";
-      medium = "#fbf1c7";
-      soft = "#f2e5bc";
-    };
     black = {
-      "-1" = "#fbf1c7";
-      "0" = "#928374";
-      "1" = "#282828";
+      "-1" = "#1d2021";
+      "0" = "#282828";
+      "1" = "#32302f";
+      "2" = "#3c3836";
+      "3" = "#504945";
+      "4" = "#665c54";
     };
-    red = {
-      "-1" = "#9d0006";
-      "0" = "#cc241d";
-      "1" = "#fb4934";
-    };
-    green = {
-      "-1" = "#79740e";
-      "0" = "#98971a";
-      "1" = "#b8bb26";
-    };
-    yellow = {
-      "-1" = "#b57614";
-      "0" = "#d79921";
-      "1" = "#fabd2f";
+    white = {
+      "-1" = "#f9f5d7";
+      "0" = "#fbf1c7";
+      "1" = "#f2e5bc";
+      "2" = "#ebdbb2";
+      "3" = "#d5c4a1";
+      "4" = "#bdae93";
     };
     blue = {
       "-1" = "#076678";
       "0" = "#458588";
       "1" = "#83a598";
     };
-    magenta = {
-      "-1" = "#8f3f71";
-      "0" = "#b16286";
-      "1" = "#d3869b";
-    };
     cyan = {
       "-1" = "#427b58";
       "0" = "#689d6a";
       "1" = "#8ec07c";
     };
-    white = {
-      "-1" = "#3c3836";
-      "0" = "#a89984";
-      "1" = "#ebdbb2";
+    gray = {
+      "-1" = "#7c6f64";
+      "0" = "#928374";
+      "1" = "#a89984";
+    };
+    green = {
+      "-1" = "#79740e";
+      "0" = "#98971a";
+      "1" = "#b8bb26";
+    };
+    magenta = {
+      "-1" = "#8f3f71";
+      "0" = "#b16286";
+      "1" = "#d3869b";
+    };
+    orange = {
+      "-1" = "#af3a03";
+      "0" = "#d65d0e";
+      "1" = "#fe8019";
+    };
+    red = {
+      "-1" = "#9d0006";
+      "0" = "#cc241d";
+      "1" = "#fb4934";
+    };
+    yellow = {
+      "-1" = "#b57614";
+      "0" = "#d79921";
+      "1" = "#fabd2f";
     };
   };
 
-  mkScheme =
+  templates.konsole =
     darkness:
     { background, bright }:
     let
-      offset = if darkness == "light" then "-1" else "1";
+      offset =
+        if background == "soft" then
+          "1"
+        else if background == "hard" then
+          "-1"
+        else
+          "0";
+      darkOrLight =
+        color:
+        if color == "black" && darkness == "light" then
+          "white"
+        else if color == "black" && darkness == "light" then
+          "white"
+        else
+          color;
       first = if bright then "dimmed" else "normal";
       second = if bright then "normal" else "bright";
     in
     {
-      background = colors.background.${darkness}.${background};
-      foreground = colors.white.${offset};
-      black.normal = colors.black.${offset};
-      black.bright = colors.black."0";
+      background = colors.${darkOrLight "black"}.${offset};
+      foreground = colors.${if darkness == "dark" then "white" else "black"}."2";
+      black.normal = colors.${darkOrLight "black"}."0";
+      black.bright = colors.gray."0";
+      white.${first} = colors.gray."0";
+      white.${second} = colors.${darkOrLight "white"}."2";
     }
     // lib.mergeAttrsList (
       map
         (c: {
           ${c} = {
             ${first} = colors.${c}."0";
-            ${second} = colors.${c}.${offset};
+            ${second} = colors.${c}.${if darkness == "dark" then "1" else "-1"};
           };
         })
         [
@@ -84,7 +106,6 @@ rec {
           "blue"
           "magenta"
           "cyan"
-          "white"
         ]
     );
 

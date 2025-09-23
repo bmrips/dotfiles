3c1cacea48eec7865b6abd07941958f3c9737d01
Benedikt Rips <benedikt.rips@gmail.com>
2025-09-24 00:00:14 +0200

    feat(precision3490): build kernel with thin LTO

--- hosts/radboud-precision3490/custom-kernel.nix
+++ hosts/radboud-precision3490/custom-kernel.nix
@@ -1,10 +1,20 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
-{
-  boot.kernelPackages = pkgs.linuxKernel.kernels.linux_latest.override {
+let
+  llvmStdenv = with pkgs; overrideCC stdenv (clang.override { inherit (llvmPackages) bintools; });
+
+  kernel = pkgs.linuxPackages_latest.kernel.override {
+    stdenv = llvmStdenv;
     extraMakeFlags = [
       "KCFLAGS+=-march=meteorlake"
       "KCFLAGS+=-mtune=meteorlake"
+      "LLVM=1"
     ];
+    ignoreConfigErrors = true;
+    structuredExtraConfig.LTO_CLANG_THIN = lib.kernel.yes;
   };
+
+in
+{
+  boot.kernelPackages = lib.mkForce (pkgs.linuxPackagesFor kernel);
 }

361b483dd1070e395595b3d96dd4fafee3dcfc80
Benedikt Rips <benedikt.rips@gmail.com>
2025-09-30 07:47:36 +0200

    feat(precision3490): enable zram-backed swap

--- hosts/radboud-precision3490.nix
+++ hosts/radboud-precision3490.nix
@@ -58,6 +58,8 @@
   services.btrfs.autoScrub.enable = true;
   services.hardware.bolt.enable = true;
 
+  zramSwap.enable = true;
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #

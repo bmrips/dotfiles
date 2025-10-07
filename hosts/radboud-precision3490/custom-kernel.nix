c72ae21d6d56bb1a44dd4093f32ea62ab6ecf31e
Benedikt Rips <benedikt.rips@gmail.com>
2025-10-07 11:38:16 +0200

    feat: configure the kernel

--- hosts/radboud-precision3490/custom-kernel.nix
+++ hosts/radboud-precision3490/custom-kernel.nix
@@ -1,7 +1,16 @@
-{ pkgs, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   llvmStdenv = with pkgs; overrideCC stdenv (clang.override { inherit (llvmPackages) bintools; });
+  kconfig = lib.kconfig {
+    inherit config;
+    cpuManufacturer = "intel";
+  };
 
   kernel = pkgs.linuxPackages_latest.kernel.override {
     stdenv = llvmStdenv;
@@ -11,10 +20,23 @@ let
       "LLVM=1"
     ];
     ignoreConfigErrors = true;
-    structuredExtraConfig.LTO_CLANG_THIN = lib.kernel.yes;
+    structuredExtraConfig = lib.mkMerge (
+      lib.flatten [
+        { LTO_CLANG_THIN = lib.kernel.yes; }
+        kconfig.base
+        kconfig.intel-only
+      ]
+    );
   };
 
 in
 {
+  # Customize the patch set in use for either adding to a tinyconfig or
+  # subtracting from defconfig
+  # boot.kernelPatches = with (import ./patches.nix {inherit lib;});
+  #   subtract ++ base ++ [ custom g14 ];
+  # boot.kernelPatches = with (import ./patches.nix {inherit lib;});
+  #   addition ++ base ++ [ custom g14 ];
+
   boot.kernelPackages = lib.mkForce (pkgs.linuxPackagesFor kernel);
 }

bd667428db88ab56cf2657a15e9dccab28e1c99b
Benedikt Rips <benedikt.rips@gmail.com>
2025-10-26 11:36:48 +0100

    refactor(git): do not guard `programs.git`

--- home-manager/config/programs/git/default.nix
+++ home-manager/config/programs/git/default.nix
@@ -9,129 +9,128 @@ let
   '';
 
 in
-lib.mkIf config.programs.git.enable {
+lib.mkMerge [
+  {
+    programs.git = {
+      signing.key = null;
+      signing.signByDefault = true;
 
-  home.shellAliases.g = "git";
+      settings = {
+        advice.detachedHead = false;
+        advice.statusHints = false;
+        core.whitespace = "tabwidth=4";
+        commit.template = "${./commit_message_template}";
+        diff.algorithm = "histogram";
+        diff.colorMoved = "default";
+        diff.renames = "copy";
+        diff.sops.textconv = "sops decrypt";
+        fetch.all = true;
+        fetch.prune = true;
+        fetch.writeCommitGraph = true;
+        init.defaultBranch = "main";
+        init.templateDir = "${./templates}";
+        log.date = "human";
+        merge.ff = "only";
+        merge.tool = "nvim";
+        pager.difftool = true;
+        pull.rebase = true;
+        push.gpgSign = "if-asked";
+        push.useForceIfIncludes = true;
+        rebase.missingCommitsCheck = "error";
+        rebase.updateRefs = true;
+        rerere.enabled = true;
+        stash.showStat = true;
+        status.showStash = true;
+        user.name = "Benedikt Rips";
+        user.email = "benedikt.rips@gmail.com";
 
-  programs.bash.initExtra = /* bash */ ''
-    function git() {
-      ${gitWrapper}
-    }
-  '';
+        # use HTTPS instead of plain Git for Github
+        url."https://github.com/".insteadOf = "git://github.com/";
 
-  programs.zsh.siteFunctions.git = gitWrapper;
-
-  programs.delta.enable = true;
-
-  programs.git = {
-    signing.key = null;
-    signing.signByDefault = true;
-
-    settings = {
-      advice.detachedHead = false;
-      advice.statusHints = false;
-      core.whitespace = "tabwidth=4";
-      commit.template = "${./commit_message_template}";
-      diff.algorithm = "histogram";
-      diff.colorMoved = "default";
-      diff.renames = "copy";
-      diff.sops.textconv = "sops decrypt";
-      fetch.all = true;
-      fetch.prune = true;
-      fetch.writeCommitGraph = true;
-      init.defaultBranch = "main";
-      init.templateDir = "${./templates}";
-      log.date = "human";
-      merge.ff = "only";
-      merge.tool = "nvim";
-      pager.difftool = true;
-      pull.rebase = true;
-      push.gpgSign = "if-asked";
-      push.useForceIfIncludes = true;
-      rebase.missingCommitsCheck = "error";
-      rebase.updateRefs = true;
-      rerere.enabled = true;
-      stash.showStat = true;
-      status.showStash = true;
-      user.name = "Benedikt Rips";
-      user.email = "benedikt.rips@gmail.com";
-
-      # use HTTPS instead of plain Git for Github
-      url."https://github.com/".insteadOf = "git://github.com/";
-
-      color = {
-        diff = {
-          frag = "magenta bold";
-          meta = "blue";
-          whitespace = "red reverse";
+        color = {
+          diff = {
+            frag = "magenta bold";
+            meta = "blue";
+            whitespace = "red reverse";
+          };
+          status = {
+            nobranch = "red bold";
+            unmerged = "blue";
+          };
         };
-        status = {
-          nobranch = "red bold";
-          unmerged = "blue";
+
+        mergetool.nvim = {
+          cmd = /* bash */ ''nvim -d "$LOCAL" "$MERGED" "$REMOTE"'';
+          trustExitCode = false;
         };
       };
 
-      mergetool.nvim = {
-        cmd = /* bash */ ''nvim -d "$LOCAL" "$MERGED" "$REMOTE"'';
-        trustExitCode = false;
+      settings.alias = {
+        a = "add";
+        b = "branch";
+        c = "commit";
+        ca = "commit --amend";
+        cf = "commit --fixup";
+        co = "checkout";
+        cp = "cherry-pick";
+        cu = "commit --amend --no-edit";
+        d = "diff";
+        ds = "diff --staged";
+        dt = "difftool";
+        l = "log";
+        lg = "log --oneline --graph";
+        lo = "log --oneline";
+        ls = "ls-files";
+        lv = "log --format='%C(auto)%h%d %s - %C(blue)%an%C(reset), %C(magenta)%ad%C(reset)'";
+        mc = "diff --name-only --diff-filter=U";
+        mt = "mergetool";
+        pushf = "push --force-with-lease";
+        r = "reset";
+        rb = "rebase";
+        rbf = "rebase --autostash --autosquash";
+        s = "status --short";
+        sl = "shortlog";
+        sw = "switch";
       };
+
+      attributes =
+        let
+          diffDrivers = {
+            bib = "bibtex";
+            "c++" = "cpp";
+            cpp = "cpp";
+            css = "css";
+            html = "html";
+            java = "java";
+            md = "markdown";
+            php = "php";
+            pl = "perl";
+            py = "python";
+            rb = "ruby";
+            rs = "rust";
+            tex = "tex";
+            xhtml = "html";
+          };
+        in
+        [ "* text=auto" ] ++ lib.mapAttrsToList (ext: driver: "*.${ext} diff=${driver}") diffDrivers;
+
+      ignores = [
+        "/.direnv/"
+        "/result"
+        "Session*.vim"
+        "taskell.md"
+      ];
     };
+  }
 
-    settings.alias = {
-      a = "add";
-      b = "branch";
-      c = "commit";
-      ca = "commit --amend";
-      cf = "commit --fixup";
-      co = "checkout";
-      cp = "cherry-pick";
-      cu = "commit --amend --no-edit";
-      d = "diff";
-      ds = "diff --staged";
-      dt = "difftool";
-      l = "log";
-      lg = "log --oneline --graph";
-      lo = "log --oneline";
-      ls = "ls-files";
-      lv = "log --format='%C(auto)%h%d %s - %C(blue)%an%C(reset), %C(magenta)%ad%C(reset)'";
-      mc = "diff --name-only --diff-filter=U";
-      mt = "mergetool";
-      pushf = "push --force-with-lease";
-      r = "reset";
-      rb = "rebase";
-      rbf = "rebase --autostash --autosquash";
-      s = "status --short";
-      sl = "shortlog";
-      sw = "switch";
-    };
-
-    attributes =
-      let
-        diffDrivers = {
-          bib = "bibtex";
-          "c++" = "cpp";
-          cpp = "cpp";
-          css = "css";
-          html = "html";
-          java = "java";
-          md = "markdown";
-          php = "php";
-          pl = "perl";
-          py = "python";
-          rb = "ruby";
-          rs = "rust";
-          tex = "tex";
-          xhtml = "html";
-        };
-      in
-      [ "* text=auto" ] ++ lib.mapAttrsToList (ext: driver: "*.${ext} diff=${driver}") diffDrivers;
-
-    ignores = [
-      "/.direnv/"
-      "/result"
-      "Session*.vim"
-      "taskell.md"
-    ];
-  };
-
-}
+  (lib.mkIf config.programs.git.enable {
+    home.shellAliases.g = "git";
+    programs.delta.enable = true;
+    programs.zsh.siteFunctions.git = gitWrapper;
+    programs.bash.initExtra = /* bash */ ''
+      function git() {
+          ${gitWrapper}
+      }
+    '';
+  })
+]

128190866037381d51a0f31df4991c7bf8603232
Benedikt Rips <benedikt.rips@gmail.com>
2025-10-28 16:14:18 +0100

    feat(hm): install packages through NixOS when available

--- home-manager/submodule.nix
+++ home-manager/submodule.nix
@@ -1,16 +1,27 @@
 {
+  config,
   host,
   inputs,
+  lib,
   system,
   user,
   ...
 }:
 
+let
+  cfg = config.home-manager;
+in
 {
   imports = [ inputs.home-manager.nixosModules.home-manager ];
 
+  environment.pathsToLink = lib.mkIf (cfg.useUserPackages && cfg.users.${user}.xdg.portal.enable) [
+    "/share/applications"
+    "/share/xdg-desktop-portal"
+  ];
+
   home-manager = {
     useGlobalPkgs = true;
+    useUserPackages = true;
     extraSpecialArgs = { inherit host inputs system; };
     users.${user} = ./.;
   };

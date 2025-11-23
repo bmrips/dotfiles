2933c35666ba814a78b6fa334e613ee99819230a
Benedikt Rips <benedikt.rips@gmail.com>
2025-11-23 17:55:25 +0100

    feat(less): add more options and vim-like keybinds

--- home-manager/config/programs/less.nix
+++ home-manager/config/programs/less.nix
@@ -1,9 +1,47 @@
+{ config, lib, ... }:
+
 {
-  programs.less.options = {
-    LONG-PROMPT = true;
-    RAW-CONTROL-CHARS = true;
-    quiet = true;
-    quit-if-one-screen = true;
-    wheel-lines = 3;
+  programs.less.options = lib.mkMerge [
+    {
+      ignore-case = true; # actually smart-case
+      incsearch = true;
+      jump-target = 3;
+      LONG-PROMPT = true;
+      quiet = true;
+      quit-if-one-screen = true;
+      RAW-CONTROL-CHARS = true;
+      save-marks = true;
+      use-color = true;
+      wheel-lines = 3;
+    }
+    # colors need to be listed after `--use-color`
+    (lib.mkAfter {
+      color = [
+        "Ekc" # information: cyan
+        "HkK" # header: gray
+        "Mkb" # marks: blue
+        "NK-" # line numbers: gray
+        "Pkw" # prompt: gray
+        "Sky" # search: yellow
+        "1kc" # submatches: alternate cyan and magenta
+        "2km"
+        "3kc"
+        "4km"
+        "5kc"
+      ];
+    })
+  ];
+
+  programs.less.config = ''
+    #command
+    \b undo-hilite
+    < left-scroll
+    > right-scroll
+    0 no-scroll
+    $ end-scroll
+  '';
+
+  home.sessionVariables = lib.mkIf config.programs.less.enable {
+    MANPAGER = "less --header=1";
   };
 }

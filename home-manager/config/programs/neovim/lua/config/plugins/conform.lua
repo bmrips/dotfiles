4b285933f66ae8e935913507b465f3cf75a081b8
Benedikt Rips <benedikt.rips@gmail.com>
2025-12-14 23:54:50 +0100

    feat(nvim): configure the keep-sorted formatter

--- home-manager/config/programs/neovim/lua/config/plugins/conform.lua
+++ home-manager/config/programs/neovim/lua/config/plugins/conform.lua
@@ -13,7 +13,7 @@ return {
       lsp_format = 'fallback',
     },
     formatters_by_ft = {
-      ['*'] = { 'treefmt', 'trim_newlines', 'trim_whitespace' },
+      ['*'] = { 'keep-sorted', 'treefmt', 'trim_newlines', 'trim_whitespace' },
       bash = { 'shfmt' },
       cabal = { 'cabal_fmt' },
       dart = { 'dart_format' },

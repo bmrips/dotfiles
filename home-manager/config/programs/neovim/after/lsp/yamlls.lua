96ce79639d44969bad484f21d14a0b9faf326604
Benedikt Rips <benedikt.rips@gmail.com>
2025-12-16 14:46:04 +0100

    chore(yamlls): latexindent's schema was upstreamed

--- home-manager/config/programs/neovim/after/lsp/yamlls.lua
+++ home-manager/config/programs/neovim/after/lsp/yamlls.lua
@@ -3,7 +3,6 @@ return {
     yaml = {
       schemas = {
         kubernetes = '*.{yml,yaml}',
-        ['https://raw.githubusercontent.com/cmhughes/latexindent.pl/main/documentation/latexindent-yaml-schema.json'] = 'latexindent.yaml',
       },
     },
   },

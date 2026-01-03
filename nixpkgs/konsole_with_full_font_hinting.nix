473baca2df386ec47864c6520712741eceb9c599
Benedikt Rips <benedikt.rips@gmail.com>
2026-01-03 19:42:58 +0100

    fix(overlays): use `fetchpatch` from the final set

--- nixpkgs/konsole_with_full_font_hinting.nix
+++ nixpkgs/konsole_with_full_font_hinting.nix
@@ -1,11 +1,11 @@
-_final: prev:
+final: prev:
 
 {
   kdePackages = prev.kdePackages.overrideScope (
     _kFinal: kPrev: {
       konsole = kPrev.konsole.overrideAttrs (prevAttrs: {
         patches = (prevAttrs.patches or [ ]) ++ [
-          (prev.fetchpatch {
+          (final.fetchpatch {
             name = "enable-full-font-hinting.patch";
             url = "https://invent.kde.org/utilities/konsole/-/commit/a6d366f1fb2e915ad8d0fb9b471a033ef35b7e37.patch";
             hash = "sha256-VBXmQUrHqOXdZzeY2QLe9Hq5u3W+O7t0s1jFzDYQwVs=";

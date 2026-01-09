c92005fdb1a417a782483b766ba0a3314fe52f4c
Benedikt Rips <benedikt.rips@gmail.com>
2026-01-10 00:13:00 +0100

    ci(treefmt): integrate nixf-diagnose

--- home-manager/modules/files.nix
+++ home-manager/modules/files.nix
@@ -5,7 +5,7 @@
     path:
     let
       base = "${config.xdg.configHome}/home-manager";
-      pathStr = builtins.toString path;
+      pathStr = toString path;
       suffix = builtins.head (builtins.match "/nix/store/[^/]+/(.*)$" pathStr);
     in
     config.lib.file.mkOutOfStoreSymlink "${base}/${suffix}";

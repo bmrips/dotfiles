36e8009418830605c3b02543612d5ab5c2e776b0
Benedikt Rips <benedikt.rips@gmail.com>
2026-01-18 18:09:21 +0100

    refactor(nvim): clear highlight groups by default

--- home-manager/config/programs/neovim/after/ftplugin/dot.lua
+++ home-manager/config/programs/neovim/after/ftplugin/dot.lua
@@ -4,7 +4,7 @@ opt.errorformat = '%EError: %f:%l:%m,%+Ccontext: %.%#,%WWarning: %m'
 opt.makeprg = 'dot -Tpdf % -o %:r.pdf'
 
 -- Write the buffer before compiling
-local augroup = vim.api.nvim_create_augroup('dot', { clear = false })
+local augroup = vim.api.nvim_create_augroup('dot', { clear = true })
 vim.api.nvim_create_autocmd('QuickFixCmdPre', {
   desc = 'Write the buffer before compiling',
   group = augroup,
--- home-manager/config/programs/neovim/lua/config/plugins/gruvbox-material.lua
+++ home-manager/config/programs/neovim/lua/config/plugins/gruvbox-material.lua
@@ -14,7 +14,7 @@ return {
     vim.api.nvim_create_autocmd('Colorscheme', {
       desc = 'Colorscheme overrides',
       pattern = 'gruvbox-material',
-      group = vim.api.nvim_create_augroup('gruvbox-material', { clear = false }),
+      group = vim.api.nvim_create_augroup('gruvbox-material', { clear = true }),
       callback = function()
         vim.api.nvim_set_hl(0, 'QuickFixLine', { link = 'Visual' })
         vim.api.nvim_set_hl(0, 'Search', { link = 'Substitute' })

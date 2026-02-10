cd8bd7ab099f27ed9be2fdfff31d184425b2ce98
Benedikt Rips <benedikt.rips@gmail.com>
2026-02-10 21:22:47 +0100

    fix(nvim): add app/, src/, and tests/ to Haskell path

--- home-manager/config/programs/neovim/after/ftplugin/haskell.lua
+++ home-manager/config/programs/neovim/after/ftplugin/haskell.lua
@@ -2,7 +2,8 @@ local opt = vim.opt_local
 
 opt.includeexpr = "substitute(v:fname, '\\.', '/', 'g')"
 opt.iskeyword:append "'"
-opt.tabstop = 2
 opt.suffixesadd = { '.hs', '.lhs' }
+opt.path:append { 'app/', 'src/', 'tests/' }
+opt.tabstop = 2
 
 vim.b.undo_ftplugin = vim.b.undo_ftplugin .. '| set includeexpr< iskeyword< tabstop< suffixesadd<'

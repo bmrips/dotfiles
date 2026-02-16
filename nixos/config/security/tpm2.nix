0a22460ad8bedbd27e56094ad9db0a5d57bfa7cb
Benedikt Rips <benedikt.rips@gmail.com>
2026-02-16 12:21:54 +0100

    feat(tpm2): enable the userspace manager and PKCS#11

--- nixos/config/security/tpm2.nix
+++ nixos/config/security/tpm2.nix
@@ -5,6 +5,15 @@
   ...
 }:
 
-lib.mkIf config.security.tpm2.enable {
-  users.users.${user}.extraGroups = [ config.security.tpm2.tssGroup ];
+{
+  security.tpm2 = {
+    abrmd.enable = true;
+    pkcs11.enable = true;
+    tctiEnvironment.enable = true;
+    tctiEnvironment.interface = "tabrmd";
+  };
+
+  users.users.${user}.extraGroups = lib.mkIf config.security.tpm2.enable [
+    config.security.tpm2.tssGroup
+  ];
 }

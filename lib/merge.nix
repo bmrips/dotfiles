b754ce159e27d6445b95a7cabae9d58eff294e04
Benedikt Rips <benedikt.rips@gmail.com>
2026-03-04 16:11:36 +0100

    fix(hm): specify output format for yq-based merges

--- lib/merge.nix
+++ lib/merge.nix
@@ -1,12 +1,15 @@
 { lib, pkgs, ... }:
 
-rec {
-  json = yaml;
-
-  yaml =
-    targetFile:
+let
+  merge =
+    format: targetFile:
     lib.concatMapStrings (file: /* bash */ ''
       [[ -r '${file}' ]] &&
-        run ${lib.getExe pkgs.yq-go} --inplace $VERBOSE_ARG '. *= load("${file}")' '${targetFile}'
+        run ${lib.getExe pkgs.yq-go} --inplace $VERBOSE_ARG --output-format ${format} '. *= load("${file}")' '${targetFile}'
     '');
+
+in
+{
+  json = merge "json";
+  yaml = merge "yaml";
 }

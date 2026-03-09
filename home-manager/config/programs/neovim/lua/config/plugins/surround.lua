3295fc8688580b03579c6059677372e17ee31320
Benedikt Rips <benedikt.rips@gmail.com>
2026-03-09 21:44:45 +0100

    chore(nvim): upgrade plugins

--- home-manager/config/programs/neovim/lua/config/plugins/surround.lua
+++ home-manager/config/programs/neovim/lua/config/plugins/surround.lua
@@ -1,19 +1,7 @@
 return {
   'kylechui/nvim-surround',
   event = 'VeryLazy',
-  opts = {
-    keymaps = {
-      insert = '<C-g>z',
-      insert_line = '<C-g>Z',
-      normal = 'yz',
-      normal_cur = 'yzz',
-      normal_line = 'yZ',
-      normal_cur_line = 'yZZ',
-      visual = 'Z',
-      visual_line = 'gZ',
-      delete = 'dz',
-      change = 'cz',
-      change_line = 'cZ',
-    },
-  },
+  init = function()
+    vim.g.nvim_surround_no_normal_mappings = true
+  end,
 }

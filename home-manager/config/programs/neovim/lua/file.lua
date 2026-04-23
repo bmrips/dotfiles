aa95eed035c9f9c0f3c21e76545741312bae35cf
Benedikt Rips <benedikt.rips@gmail.com>
2026-04-23 11:42:34 +0200

    refactor(nvim): check file existence with `uv.fs_stat`

--- home-manager/config/programs/neovim/lua/file.lua
+++ home-manager/config/programs/neovim/lua/file.lua
@@ -1,20 +1,9 @@
 local M = {}
 
--- Check whether a file exists.
-function M.exists(name)
-  local f = io.open(name, 'r')
-  if f ~= nil then
-    io.close(f)
-    return true
-  else
-    return false
-  end
-end
-
 -- Returns the given string if there is no Makefile, otherwise returns the empty
 -- string.
 function M.makeOr(str)
-  if M.exists 'makefile' or M.exists 'Makefile' then
+  if vim.uv.fs_stat 'makefile' or vim.uv.fs_stat 'Makefile' then
     return ''
   else
     return str

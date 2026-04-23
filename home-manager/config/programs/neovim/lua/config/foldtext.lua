c55004f8d2924edba869d5c805417af209aef3a5
Benedikt Rips <benedikt.rips@gmail.com>
2026-04-23 10:52:31 +0200

    chore(nvim): flatten my config

--- home-manager/config/programs/neovim/lua/config/foldtext.lua
+++
@@ -1,5 +0,0 @@
-return function(linenr)
-  local line = vim.api.nvim_buf_get_lines(0, linenr - 1, linenr, true)[1]
-  local tabInSpaces = string.rep(' ', vim.opt.tabstop:get())
-  return line:gsub('%s*$', ' ⋯'):gsub('\t', tabInSpaces)
-end
--- home-manager/config/programs/neovim/lua/fold.lua
+++ home-manager/config/programs/neovim/lua/fold.lua
@@ -8,7 +8,7 @@ local addMark = function(linenr, fdm, level)
   vim.api.nvim_buf_set_lines(0, linenr - 1, linenr, true, { new_line })
 end
 
--- Create a fold for line1..line2 at the given level.
+-- Create a fold for `line1..line2` at the given level.
 function M.create(info)
   local fdm = vim.opt_local.foldmarker:get()
   local level = (info.args ~= '0' and info.args) or ''
@@ -16,6 +16,13 @@ function M.create(info)
   if info.line1 ~= info.line2 then
     addMark(info.line2, fdm[2], level)
   end
+end
+
+-- Generate the fold text for the fold at the given line.
+function M.text(linenr)
+  local line = vim.api.nvim_buf_get_lines(0, linenr - 1, linenr, true)[1]
+  local tabInSpaces = string.rep(' ', vim.opt.tabstop:get())
+  return line:gsub('%s*$', ' ⋯'):gsub('\t', tabInSpaces)
 end
 
 return M

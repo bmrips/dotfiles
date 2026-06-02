55fa11ff41c280f8804175672172a66ff6d8348a
Benedikt Rips <benedikt.rips@gmail.com>
2026-06-02 13:00:15 +0200

    chore(nvim): remove the emoji completion source

--- home-manager/config/programs/neovim/lua/plugins/blink.cmp.lua
+++ home-manager/config/programs/neovim/lua/plugins/blink.cmp.lua
@@ -1,59 +1,38 @@
 ---@module 'lazy.types'
 ---@type LazyPluginSpec[]
 return {
-  {
-    'saghen/blink.cmp',
-    event = { 'CmdlineEnter', 'InsertEnter' },
-    dependencies = 'rafamadriz/friendly-snippets',
+  'saghen/blink.cmp',
+  event = { 'CmdlineEnter', 'InsertEnter' },
+  dependencies = 'rafamadriz/friendly-snippets',
 
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      appearance = { nerd_font_variant = 'mono' },
-      completion = {
-        documentation = { auto_show = true },
-        list = {
-          selection = { preselect = false },
-        },
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    appearance = { nerd_font_variant = 'mono' },
+    completion = {
+      documentation = { auto_show = true },
+      list = {
+        selection = { preselect = false },
       },
-      fuzzy = {
-        prebuilt_binaries = { download = false },
-      },
-      keymap = { preset = 'enter' },
-      signature = { enabled = true },
-      sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
-        providers = {
-          snippets = {
-            opts = {
-              extended_filetypes = {
-                dart = { 'flutter' },
-              },
+    },
+    fuzzy = {
+      prebuilt_binaries = { download = false },
+    },
+    keymap = { preset = 'enter' },
+    signature = { enabled = true },
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      providers = {
+        snippets = {
+          opts = {
+            extended_filetypes = {
+              dart = { 'flutter' },
             },
           },
         },
       },
     },
-    opts_extend = { 'sources.default' },
   },
-  {
-    'saghen/blink.cmp',
-    optional = true,
-    dependencies = 'moyiz/blink-emoji.nvim',
 
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      sources = {
-        default = { 'emoji' },
-        providers = {
-          emoji = {
-            module = 'blink-emoji',
-            name = 'Emoji',
-            score_offset = 15,
-          },
-        },
-      },
-    },
-  },
+  opts_extend = { 'sources.default' },
 }

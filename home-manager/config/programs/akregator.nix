4cc354d19e8e2224eacaf315b44bdccbbbfe16fd
Benedikt Rips <benedikt.rips@gmail.com>
2026-06-14 22:30:56 +0200

    feat(akregator): declare feeds through new option

--- home-manager/config/programs/akregator.nix
+++ home-manager/config/programs/akregator.nix
@@ -3,82 +3,108 @@
 lib.mkMerge [
 
   {
-    programs.akregator.shortcutSchemes.Custom = {
-      "open_kcommand_bar" = "Ctrl+Alt+I";
-      "options_configure" = "Ctrl+&lt;";
-      "options_configure_keybinding" = "Ctrl+>";
-      "options_show_menubar" = "Ctrl+M";
-      "file_quit" = "Ctrl+Q";
-      "hamburger_menu" = "F10";
-      "fullscreen" = "F11";
-      "help_whats_this" = "Shift+F1";
-      "help_about_app" = "F1";
+    programs.akregator = {
 
-      "find_in_messages" = "/";
-      "focus_to_quickseach" = "S";
+      feeds = {
+        "CS SYD" = {
+          comment = "Tom Sydney Kerckhove";
+          homepage = "https://cs-syd.eu/";
+          type = "RSS";
+          url = "https://cs-syd.eu/atom.xml";
+        };
+        "KDE Blogs" = {
+          description = "Recent content on KDE Blogs";
+          homepage = "https://blogs.kde.org/";
+          type = "RSS";
+          url = "https://blogs.kde.org/index.xml";
+        };
+        "Left Fold" = {
+          comment = "Johann Zuber";
+          description = "A personal blog about programming topics I enjoy";
+          homepage = "https://www.leftfold.tech/";
+          type = "RSS";
+          url = "https://www.leftfold.tech/feed.xml";
+        };
+      };
 
-      "normal_view" = "Ctrl+1";
-      "combined_view" = "Ctrl+2";
-      "widescreen_view" = "Ctrl+3";
+      shortcutSchemes.Custom = {
+        "open_kcommand_bar" = "Ctrl+Alt+I";
+        "options_configure" = "Ctrl+&lt;";
+        "options_configure_keybinding" = "Ctrl+>";
+        "options_show_menubar" = "Ctrl+M";
+        "file_quit" = "Ctrl+Q";
+        "hamburger_menu" = "F10";
+        "fullscreen" = "F11";
+        "help_whats_this" = "Shift+F1";
+        "help_about_app" = "F1";
 
-      "go_previous_article" = "K; ";
-      "go_next_article" = "J";
-      "go_prev_feed" = "Ctrl+K";
-      "go_next_feed" = "Ctrl+J";
-      "go_prev_unread_article" = "H";
-      "go_next_unread_article" = "L";
-      "go_next_unread_feed" = "Ctrl+L";
-      "go_prev_unread_feed" = "Ctrl+H";
+        "find_in_messages" = "/";
+        "focus_to_quickseach" = "S";
 
-      "article_open" = "Return";
-      "article_open_in_background" = "Ctrl+Return";
-      "article_open_external" = "Shift+Return";
+        "normal_view" = "Ctrl+1";
+        "combined_view" = "Ctrl+2";
+        "widescreen_view" = "Ctrl+3";
 
-      "article_set_status_important" = "I";
-      "article_set_status_new" = "N";
-      "article_set_status_read" = "R";
-      "article_set_status_unread" = "U";
+        "go_previous_article" = "K; ";
+        "go_next_article" = "J";
+        "go_prev_feed" = "Ctrl+K";
+        "go_next_feed" = "Ctrl+J";
+        "go_prev_unread_article" = "H";
+        "go_next_unread_article" = "L";
+        "go_next_unread_feed" = "Ctrl+L";
+        "go_prev_unread_feed" = "Ctrl+H";
 
-      "feed_add" = "Ctrl+N";
-      "feed_fetch_all" = "P";
-      "feed_hide_read" = ".";
-      "feed_homepage" = "O";
-      "feed_mark_all_as_read" = "Ctrl+R";
-      "feed_mark_all_feeds_as_read" = "Ctrl+Shift+R";
-      "feed_modify" = "F2";
+        "article_open" = "Return";
+        "article_open_in_background" = "Ctrl+Return";
+        "article_open_external" = "Shift+Return";
 
-      "feedstree_home" = "Home";
-      "feedstree_end" = "End";
-      "feedstree_left" = "Left";
-      "feedstree_right" = "Right";
-      "feedstree_up" = "Up";
-      "feedstree_down" = "Down";
+        "article_set_status_important" = "I";
+        "article_set_status_new" = "N";
+        "article_set_status_read" = "R";
+        "article_set_status_unread" = "U";
 
-      "tab_remove" = "Ctrl+W";
-      "select_next_tab" = [
-        "Ctrl+."
-        "Ctrl+Tab"
-      ];
-      "select_previous_tab" = [
-        "Ctrl+,"
-        "Ctrl+Shift+Tab"
-      ];
-      "activate_tab_01" = "Alt+1";
-      "activate_tab_02" = "Alt+2";
-      "activate_tab_03" = "Alt+3";
-      "activate_tab_04" = "Alt+4";
-      "activate_tab_05" = "Alt+5";
-      "activate_tab_06" = "Alt+6";
-      "activate_tab_07" = "Alt+7";
-      "activate_tab_08" = "Alt+8";
-      "activate_tab_09" = "Alt+9";
+        "feed_add" = "Ctrl+N";
+        "feed_fetch_all" = "P";
+        "feed_hide_read" = ".";
+        "feed_homepage" = "O";
+        "feed_mark_all_as_read" = "Ctrl+R";
+        "feed_mark_all_feeds_as_read" = "Ctrl+Shift+R";
+        "feed_modify" = "F2";
 
-      "viewer_copy" = "Ctrl+C";
-      "viewer_print" = "Ctrl+P";
+        "feedstree_home" = "Home";
+        "feedstree_end" = "End";
+        "feedstree_left" = "Left";
+        "feedstree_right" = "Right";
+        "feedstree_up" = "Up";
+        "feedstree_down" = "Down";
 
-      "zoom_in" = "=; +";
-      "zoom_out" = "-";
-      "zoom_reset" = "0";
+        "tab_remove" = "Ctrl+W";
+        "select_next_tab" = [
+          "Ctrl+."
+          "Ctrl+Tab"
+        ];
+        "select_previous_tab" = [
+          "Ctrl+,"
+          "Ctrl+Shift+Tab"
+        ];
+        "activate_tab_01" = "Alt+1";
+        "activate_tab_02" = "Alt+2";
+        "activate_tab_03" = "Alt+3";
+        "activate_tab_04" = "Alt+4";
+        "activate_tab_05" = "Alt+5";
+        "activate_tab_06" = "Alt+6";
+        "activate_tab_07" = "Alt+7";
+        "activate_tab_08" = "Alt+8";
+        "activate_tab_09" = "Alt+9";
+
+        "viewer_copy" = "Ctrl+C";
+        "viewer_print" = "Ctrl+P";
+
+        "zoom_in" = "=; +";
+        "zoom_out" = "-";
+        "zoom_reset" = "0";
+      };
+
     };
   }
 
---
+++ home-manager/modules/programs/akregator/default.nix
@@ -0,0 +1,218 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.programs.akregator;
+
+  feed =
+    { name, ... }:
+    {
+      options = {
+        archiving = {
+          mode = lib.mkOption {
+            description = "The archiving mode for this feed.";
+            example = "keepAllArticles";
+            default = "globalDefault";
+            type = lib.types.enum [
+              "globalDefault"
+              "keepAllArticles"
+              "limitArticleNumber"
+              "limitArticleAge"
+              "disableArchiving"
+            ];
+          };
+          maxArticleAge = lib.mkOption {
+            description = "The maximum age of articles to be kept (in days).";
+            example = "60";
+            type = lib.types.ints.positive;
+          };
+          maxArticleNumber = lib.mkOption {
+            description = "The maximum number of articles to keep.";
+            example = "1000";
+            type = lib.types.ints.positive;
+          };
+        };
+        comment = lib.mkOption {
+          description = "A comment attached to this feed.";
+          default = null;
+          type = with lib.types; nullOr str;
+        };
+        description = lib.mkOption {
+          description = "Description of this feed.";
+          example = "Recent content on KDE Blogs";
+          default = "";
+          type = with lib.types; nullOr str;
+        };
+        homepage = lib.mkOption {
+          description = ''
+            The homepage of this feed.
+
+            Corresponds to the `htmlUrl` field.
+          '';
+          example = "https://blogs.kde.org/";
+          default = null;
+          type = with lib.types; nullOr str;
+        };
+        loadFullWebsite = lib.mkOption {
+          description = ''
+            Whether to load the full website when viewing an article.
+
+            Corresponds to the `loadLinkedWebsite` field.
+          '';
+          example = true;
+          default = null;
+          type = with lib.types; nullOr bool;
+        };
+        markImmediatelyAsRead = lib.mkOption {
+          description = "Whether to mark articles as read on arrival.";
+          example = true;
+          default = null;
+          type = with lib.types; nullOr bool;
+        };
+        name = lib.mkOption {
+          description = "The name of the feed. This determines the `text` and `title` fields.";
+          example = "KDE Blogs";
+          default = name;
+          defaultText = lib.literalExpression "<name>";
+          type = lib.types.str;
+        };
+        notify = lib.mkOption {
+          description = ''
+            Whether to send a notification on article arrival.
+
+            Corresponds to the `useNotification` field.
+          '';
+          example = true;
+          default = null;
+          type = with lib.types; nullOr bool;
+        };
+        type = lib.mkOption {
+          description = ''
+            The type of this feed.
+
+            Corresponds to the `type` and `version` fields.
+          '';
+          type = lib.types.enum [
+            "RSS"
+            "Atom"
+          ];
+        };
+        updateInterval = lib.mkOption {
+          description = ''
+            The interval (in minutes) with which the feed is updated. Set to `null`
+            to use the global default.
+
+            Corresponds to the `useCustomFetchInterval` and `fetchInterval` fields.
+          '';
+          example = "60";
+          default = null;
+          type = with lib.types; nullOr ints.positive;
+        };
+        url = lib.mkOption {
+          description = ''
+            The URL from where to fetch the feed.
+
+            Corresponds to the `xmlUrl` field.
+          '';
+          example = "https://blogs.kde.org/index.xml";
+          type = lib.types.str;
+        };
+      };
+    };
+
+  mkFeed =
+    feed:
+    let
+      setIfNonNull =
+        option: attribute:
+        lib.optionalAttrs (option != null) {
+          ${attribute} = option;
+        };
+    in
+    {
+      "@archiveMode" = feed.archiving.mode;
+      "@text" = feed.name;
+      "@title" = feed.name;
+      "@type" = lib.toLower feed.type;
+      "@useCustomFetchInterval" = feed.updateInterval != null;
+      "@version" = feed.type;
+      "@xmlUrl" = feed.url;
+    }
+    // lib.optionalAttrs (feed.archiving.mode == "limitArticleAge") {
+      "@maxArticleAge" = feed.archiving.maxArticleAge;
+    }
+    // lib.optionalAttrs (feed.archiving.mode == "limitArticleNumber") {
+      "@maxArticleNumber" = feed.archiving.maxArticleNumber;
+    }
+    // lib.optionalAttrs (feed.updateInterval != null) {
+      "@fetchInterval" = feed.updateInterval;
+    }
+    // setIfNonNull feed.comment "@comment"
+    // setIfNonNull feed.description "@description"
+    // setIfNonNull feed.homepage "@htmlUrl"
+    // setIfNonNull feed.loadFullWebsite "@loadLinkedWebsite"
+    // setIfNonNull feed.markImmediatelyAsRead "@markImmediatelyAsRead"
+    // setIfNonNull feed.notify "@useNotification";
+
+  xml = pkgs.formats.xml { };
+in
+{
+  options.programs.akregator = {
+    enable = lib.mkEnableOption "Akregator.";
+
+    package = lib.mkPackageOption pkgs [ "kdePackages" "akregator" ] { nullable = true; };
+
+    shortcutSchemes = lib.plasma.shortcutSchemesOption;
+
+    feeds = lib.mkOption {
+      description = "Feeds that Akregator fetches.";
+      example."KDE Blogs" = {
+        description = "Recent content on KDE Blogs";
+        homepage = "https://blogs.kde.org/";
+        type = "RSS";
+        url = "https://blogs.kde.org/index.xml";
+      };
+      default = { };
+      type = with lib.types; attrsOf (submodule feed);
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    assertions = lib.mapAttrsToList (key: feed: {
+      assertion = feed.homepage != null -> lib.hasPrefix feed.homepage feed.url;
+      message =
+        let
+          optionPath = lib.showOption [
+            "programs"
+            "akregator"
+            "feeds"
+            key
+          ];
+        in
+        ''
+          For the Akregator feed '${optionPath}', the homepage is
+          not a prefix of the feed URL:
+          - homepage: '${feed.homepage}'
+          - URL: '${feed.url}'
+        '';
+    }) cfg.feeds;
+
+    home.packages = lib.mkIf (cfg.package != null) [ cfg.package ];
+
+    programs.plasma.shortcutSchemes.akregator = cfg.shortcutSchemes;
+
+    home.file'.".local/share/akregator/data/feeds.opml" = {
+      type = "xml";
+      mergeParams.stylesheet = ./merge-feeds.xslt;
+      sources = [
+        (xml.generate "akregator-feeds.xml" {
+          opml.body.outline = map mkFeed (lib.attrValues cfg.feeds);
+        })
+      ];
+    };
+  };
+}

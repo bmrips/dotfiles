e3df5753fb41656be3a0a9d6ff762ef4f844ac09
Benedikt Rips <benedikt.rips@gmail.com>
2026-07-13 12:54:50 +0200

    refactor(lazygit): make integrations dependent

--- home-manager/config/programs/lazygit.nix
+++ home-manager/config/programs/lazygit.nix
@@ -3,25 +3,30 @@
 {
   home.shellAliases.lg = lib.mkIf config.programs.lazygit.enable "lazygit";
 
-  programs.lazygit.settings = {
-    git = {
-      overrideGpg = true;
-      pagers = [
+  programs.lazygit.settings = lib.mkMerge [
+
+    {
+      git.overrideGpg = true;
+      gui = {
+        authorColors = {
+          "Benedikt Rips" = "cyan";
+          "Rips, Benedikt" = "cyan";
+        };
+        nerdFontsVersion = "3";
+        skipAmendWarning = true;
+        skipDiscardChangeWarning = true;
+        skipStashWarning = true;
+        skipRewordInEditorWarning = true;
+      };
+      promptToReturnFromSubprocess = false;
+    }
+
+    (lib.mkIf config.programs.delta.enable {
+      git.pagers = [
         { pager = "delta --paging=never --width=-1"; }
         { pager = "delta --paging=never --width=-1 --side-by-side"; }
       ];
-    };
-    gui = {
-      authorColors = {
-        "Benedikt Rips" = "cyan";
-        "Rips, Benedikt" = "cyan";
-      };
-      nerdFontsVersion = "3";
-      skipAmendWarning = true;
-      skipDiscardChangeWarning = true;
-      skipStashWarning = true;
-      skipRewordInEditorWarning = true;
-    };
-    promptToReturnFromSubprocess = false;
-  };
+    })
+
+  ];
 }

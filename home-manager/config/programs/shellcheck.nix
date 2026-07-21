3ac3f37ddeffbe2320f4f83449f5caca6443f677
Benedikt Rips <benedikt.rips@gmail.com>
2026-07-21 08:55:42 +0200

    feat(shellcheck): enable more optional checks

--- home-manager/config/programs/shellcheck.nix
+++ home-manager/config/programs/shellcheck.nix
@@ -4,11 +4,14 @@
     enable = [
       # enable optional checks
       "add-default-case"
+      "avoid-negated-conditions"
       "avoid-nullary-conditions"
       "check-deprecate-which"
+      "check-extra-masked-returns"
       "check-set-e-suppressed"
       "deprecate-which"
       "require-double-brackets"
+      "useless-use-of-cat"
     ];
   };
 }

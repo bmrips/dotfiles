b8983327c863934f0c50a061bcd1051bf646ac7c
Benedikt Rips <benedikt.rips@gmail.com>
2026-09-08 13:33:25 +0200

    feat: use nixpkgs overlays exposed in the flake

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,8 +1,7 @@
+{ inputs, lib, ... }:
+
 {
   nixpkgs.config.allowUnfree = true;
 
-  nixpkgs.overlays = [
-    (import ./packages/overlay.nix)
-    (import ./konsole-with-split-view-shortcuts.nix)
-  ];
+  nixpkgs.overlays = lib.attrValues inputs.self.overlays;
 }

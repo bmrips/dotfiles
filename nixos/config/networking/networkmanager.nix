eea97da3cb3b66cd0ba73379a9336a2d31896426
Benedikt Rips <benedikt.rips@gmail.com>
2026-09-13 22:44:15 +0200

    fix(NetworkManager): wait for a connection to be up

--- nixos/config/networking/networkmanager.nix
+++ nixos/config/networking/networkmanager.nix
@@ -5,32 +5,44 @@
   ...
 }:
 
-{
-  users.users.${user}.extraGroups = lib.mkIf config.networking.networkmanager.enable [
-    "networkmanager"
-  ];
-
-  networking.networkmanager.ensureProfiles.profiles = {
-    Home = {
-      connection.id = "Home";
-      connection.type = "wifi";
-      wifi.ssid = "$HOME_SSID";
-      wifi-security = {
-        key-mgmt = "sae";
-        psk = "$HOME_PSK";
+let
+  cfg = config.networking.networkmanager;
+in
+lib.mkMerge [
+  {
+    networking.networkmanager.ensureProfiles.profiles = {
+      Home = {
+        connection.id = "Home";
+        connection.type = "wifi";
+        wifi.ssid = "$HOME_SSID";
+        wifi-security = {
+          key-mgmt = "sae";
+          psk = "$HOME_PSK";
+        };
+      };
+      Phone = {
+        connection.id = "Phone";
+        connection.type = "wifi";
+        wifi.ssid = "$PHONE_SSID";
+        wifi-security = {
+          key-mgmt = "wpa-psk";
+          psk = "$PHONE_PSK";
+        };
       };
     };
-    Phone = {
-      connection.id = "Phone";
-      connection.type = "wifi";
-      wifi.ssid = "$PHONE_SSID";
-      wifi-security = {
-        key-mgmt = "wpa-psk";
-        psk = "$PHONE_PSK";
-      };
-    };
-  };
 
-  sops.secrets.networks = lib.mkIf config.networking.networkmanager.enable { };
-  networking.networkmanager.ensureProfiles.environmentFiles = config.lib.sops.pathOptional "networks";
-}
+    sops.secrets.networks = lib.mkIf cfg.enable { };
+    networking.networkmanager.ensureProfiles.environmentFiles = config.lib.sops.pathOptional "networks";
+  }
+
+  (lib.mkIf cfg.enable {
+    users.users.${user}.extraGroups = [ "networkmanager" ];
+
+    # Overwrite NetworkManager-wait-online.service's ExecStart to remove the
+    # `-s` flag such that the service succeeds only when a connection is up.
+    systemd.services.NetworkManager-wait-online.serviceConfig.ExecStart = [
+      ""
+      "${cfg.package}/bin/nm-online -q"
+    ];
+  })
+]

4d1280a00c0d939d1a45b08c3f9662263536d401
Benedikt Rips <benedikt.rips@gmail.com>
2026-09-17 09:40:58 +0200

    Revert "chore: remove the bt-dualboot package and module"
    This reverts commit be84325cb302b56be260cce283f52be7dc1b448f.

--- nixpkgs/packages/default.nix
+++ nixpkgs/packages/default.nix
@@ -1,6 +1,7 @@
 pkgs:
 
 {
+  bt-dualboot = pkgs.callPackage ./bt-dualboot.nix { };
   fzf-tab-completion = pkgs.callPackage ./fzf-tab-completion.nix { };
   rl_custom_function = pkgs.callPackage ./rl_custom_function.nix { };
 }
